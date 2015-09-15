a906cbd2b233690a9d66b522a0265d2f97375f1c
yure0000 <yure0000@YURENA.esri.com>
2015-09-15 11:25:57 +0100

    Quartz release Map Samples

--- src/main/java/com/esri/sampleviewer/samples/map/ChangeBasemap.java
+++ src/main/java/com/esri/sampleviewer/samples/map/ChangeBasemap.java
@@ -1,11 +1,11 @@
 /* Copyright 2015 Esri.
-
+ 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
+ 
     http://www.apache.org/licenses/LICENSE-2.0
-
+ 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,121 +14,113 @@ limitations under the License.  */
 
 package com.esri.sampleviewer.samples.map;
 
-
-import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-
-import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.BasemapType;
 import com.esri.arcgisruntime.mapping.Map;
 import com.esri.arcgisruntime.mapping.view.MapView;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
 
 public class ChangeBasemap extends Application {
 
-    private MapView mapView;
-    private Map map;
+  private MapView mapView;
+  private Map map;
 
-    // set the strings for the combo box
-    private final String streets = "Streets";
-    private final String topo = "Topographic";
-    private final String gray = "Gray";
-    private final String oceans = "Oceans";
+  private static final double LATITUDE = 57.5000;
+  private static final double LONGITUDE = -5.0000;
+  private static final int LOD = 6;
 
-    @Override
-    public void start(Stage stage) {
-        // create a border pane
-        BorderPane borderPane = new BorderPane();
-        Scene scene = new Scene(borderPane);
+  /**
+   * This application shows how to create a ({@link Map}) using a
+   * {@link BasemapType} instance, giving you the option to easily set a map
+   * type (base layer), latitude and longitude around which to center the map,
+   * and zoom level for the map. The BasemapType class is then used to switch
+   * the type of basemap in the map on-the-fly.
+   */
+  @Override
+  public void start(Stage stage) throws Exception {
 
-        // size the stage and add a title
-        stage.setTitle("Change Basemaps Sample");
-        stage.setWidth(800);
-        stage.setHeight(600);
+    // creates a border pane and application scene
+    BorderPane borderPane = new BorderPane();
+    Scene scene = new Scene(borderPane);
 
-        // create a HBox layout pane
-        HBox hbox = new HBox();
-        hbox.setPadding(new Insets(5, 5, 5, 5));
-        hbox.setSpacing(10);
-        // set background primary color blue
-        hbox.setStyle("-fx-background-color: #2196F3");
-        // create a label
-        Label label = new Label("Choose Basemap: ");
-        label.setTextFill(Color.web("#FFFFFF"));
+    // size the stage and add a title
+    stage.setTitle("Change Basemap");
+    stage.setWidth(700);
+    stage.setHeight(800);
+    stage.setScene(scene);
+    stage.show();
 
-        // create a combo box
-        ComboBox basemapComboBox = new ComboBox();
-        basemapComboBox.getItems().addAll(streets, topo, gray, oceans);
-        basemapComboBox.getSelectionModel().select(topo);
-        basemapComboBox.setEditable(false);
-        // add change listener on combo box
-        basemapComboBox.valueProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue ov, String value, String name) {
-                // handle combo box item selection
-                switch (name) {
-                    case streets:
-                        // create a map with Streets Basemap
-                        map.setBasemap(Basemap.createStreets());
-                        break;
-                    case topo:
-                        // create a map with Topographic Basemap
-                        map.setBasemap(Basemap.createTopographic());
-                        break;
-                    case gray:
-                        // create a map with Gray Basemap
-                        map.setBasemap(Basemap.createLightGrayCanvas());
-                        break;
-                    case oceans:
-                        // create a map with Oceans Basemap
-                        map.setBasemap(Basemap.createOceans());
-                        break;
-                    default:
-                        System.out.println(name);
-                }
-            }
+    TilePane tilePane = new TilePane();
+    tilePane.setHgap(8);
+    tilePane.setAlignment(Pos.CENTER);
+    borderPane.setTop(tilePane);
+
+    for (BasemapType type : BasemapType.values()) {
+      if (type != BasemapType.UNKNOWN) {
+        String basemapString = type.toString();
+        Button button = new Button();
+        button.setTooltip(new Tooltip(basemapString));
+        button.setGraphic(new ImageView(new Image(getClass()
+            .getResourceAsStream("resources/" + basemapString + ".png"))));
+        // listener to switch map types when button clicked
+        button.setOnAction(e -> {
+          if (map != null) {
+            mapView.setMap(null);
+            map.dispose();
+          }
+          map = new Map(BasemapType.valueOf(basemapString), LATITUDE,
+              LONGITUDE, LOD);
+          mapView.setMap(map);
         });
 
-        // add lable and combo box to hbox
-        hbox.getChildren().addAll(label, basemapComboBox);
-
-    // make a new map using Topographic mapping centred over East Scotland at
-    // zoom level 10.
-        map = new Map(BasemapType.TOPOGRAPHIC, 56.075844,-2.681572, 10);
-
-        // create the MapView JavaFX control and assign its map
-        mapView = new MapView();
-        mapView.setMap(map);
-
-        // set the panes to the border
-        borderPane.setTop(hbox);
-        borderPane.setCenter(mapView);
-        // show stage
-        stage.setScene(scene);
-        stage.show();
-
+        tilePane.getChildren().add(button);
+      }
     }
 
-    @Override
-    public void stop() throws Exception {
-        // release resources when the application closes
-        mapView.dispose();
-        map.dispose();
-        System.exit(0);
+    // creates a Map which defines the layers of data to view
+    try {
+      // creates a new map: topographic map, centered at lat-lon 57.5N 5W
+      // (Scotland), zoom level 6
+      map = new Map(BasemapType.TOPOGRAPHIC, LATITUDE, LONGITUDE, LOD);
 
+      // creates the MapView JavaFX control and assign its map
+      mapView = new MapView();
+      mapView.setMap(map);
+
+      // adds the MapView
+      borderPane.setCenter(mapView);
+
+    } catch (Exception e) {
+      // on any error, display the stack trace.
+      e.printStackTrace();
     }
+  }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  @Override
+  public void stop() throws Exception {
+    // releases resources when the application closes
+    mapView.dispose();
+    map.dispose();
+    Platform.exit();
+    System.exit(0);
+  }
 
+  /**
+   * Starting point of this application.
+   * 
+   * @args arguments to this application.
+   */
+  public static void main(String[] args) {
+    Application.launch(args);
+  }
 }
---
+++ src/main/java/com/esri/sampleviewer/samples/map/MapInitialExtent.java
@@ -0,0 +1,105 @@
+/* Copyright 2015 Esri.
+ 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ 
+    http://www.apache.org/licenses/LICENSE-2.0
+ 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+limitations under the License.  */
+
+package com.esri.sampleviewer.samples.map;
+
+import com.esri.arcgisruntime.geometry.Envelope;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
+import com.esri.arcgisruntime.layers.ArcGISTiledLayer;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.Map;
+import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.mapping.view.Viewpoint;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MapInitialExtent extends Application {
+
+  private MapView mapView;
+  private Map map;
+
+  private final String WORLD_TOPO_SERVICE = "http://services.arcgisonline.com/arcgis/rest/services/World_Topo_Map/MapServer";
+
+  /**
+   * The Set Initial Map Area sample demonstrates how to start a <@Map> app with
+   * a defined initial area using a <@Viewpoint> created with an <@Envelope>
+   * defining the initial area.
+   */
+  @Override
+  public void start(Stage stage) throws Exception {
+
+    // creates a border pane and application scene
+    BorderPane borderPane = new BorderPane();
+    Scene scene = new Scene(borderPane);
+
+    // size the stage and add a title
+    stage.setTitle("Set Initial Map Area");
+    stage.setWidth(700);
+    stage.setHeight(800);
+    stage.setScene(scene);
+    stage.show();
+
+    try {
+      // create a map view
+      mapView = new MapView();
+      // create new Tiled Layer from service url
+      ArcGISTiledLayer topoBasemap = new ArcGISTiledLayer(WORLD_TOPO_SERVICE);
+      // set tiled layer as basemap
+      Basemap basemap = new Basemap(topoBasemap);
+      // create a map with the basemap
+      map = new Map(basemap);
+
+      // create an initial extent envelope
+      Envelope initialExtent = new Envelope(-12211308.778729, 4645116.003309,
+          -12208257.879667, 4650542.535773, 0.0, 0.0, 0.0, 0.0,
+          SpatialReferences.getWebMercator());
+
+      // create a viewpoint from envelope
+      Viewpoint viewPoint = new Viewpoint(initialExtent);
+      // set initial map extent
+      map.setInitialViewpoint(viewPoint);
+
+      // set the map to be displayed in this view
+      mapView.setMap(map);
+
+      borderPane.setCenter(mapView);
+
+    } catch (Exception e) {
+      // on any error, display the stack trace.
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void stop() throws Exception {
+    // releases resources when the application closes
+    mapView.dispose();
+    map.dispose();
+    Platform.exit();
+    System.exit(0);
+  }
+
+  /**
+   * Starting point of this application.
+   * 
+   * @args arguments to this application.
+   */
+  public static void main(String[] args) {
+    Application.launch(args);
+  }
+}
