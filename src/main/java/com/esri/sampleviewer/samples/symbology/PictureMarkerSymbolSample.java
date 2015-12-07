3329dc51cd388ca356fdd2d09db4619ab8c3e7dd
john0005 <john0005@10.195.23.87>
2015-12-07 16:23:55 +0000

    Adding Picture Marker Symbol to Public repo

--- src/main/java/com/esri/sampleviewer/samples/symbology/PictureMarkerSymbolSample.java
+++ src/main/java/com/esri/sampleviewer/samples/symbology/PictureMarkerSymbolSample.java
@@ -1,22 +1,37 @@
-/* Copyright 2015 Esri.
- 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
- 
-    http://www.apache.org/licenses/LICENSE-2.0
- 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
-limitations under the License.  */
+/*
++ * Copyright 2015 Esri.
++ *
++ * Licensed under the Apache License, Version 2.0 (the "License");
++ * you may not use this file except in compliance with the License.
++ * You may obtain a copy of the License at
++ *
++ * http://www.apache.org/licenses/LICENSE-2.0
++ *
++ * Unless required by applicable law or agreed to in writing, software
++ * distributed under the License is distributed on an "AS IS" BASIS,
++ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
++ * See the License for the specific language governing permissions and
++ * limitations under the License.
++ */
 
 package com.esri.sampleviewer.samples.symbology;
 
 import java.io.File;
 
 import javax.imageio.ImageIO;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import com.esri.arcgisruntime.geometry.Envelope;
 import com.esri.arcgisruntime.geometry.Point;
@@ -28,71 +43,111 @@ import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.symbology.PictureMarkerSymbol;
 
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.embed.swing.SwingFXUtils;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
-
 /**
- * This sample shows how to create a <@PictureMarkerSymbol> from the different
- * types of picture resources that are available; sourced from a URL, locally on
- * the application, or from storage on disk. How it works, the three
- * PictureMarkerSymbols that you see in the application are all constructed from
- * different types of resources and then added to a <@Graphic> which is then
- * added to a <@GraphicsOverlay>. The campsite icon is constructed from a URL,
- * because this is a remote resource the symbol needs to be loaded before it is
- * added to a Graphic and added to the <@Map>. The blue pin with a star is
- * created from an application resource called a Image, these also need to be
- * loaded before they are added to the map. The orange pin is created from a
- * file path on disk (which is written to disk when the application starts and
- * cleaned up when the application closes).
+ * This sample demonstrates how to create a PictureMarkerSymbol from a URL,
+ * locally on the application, or from disk storage.
+ * <p>
+ * The {@link GraphicsOverlay} will hold our {@link PictureMarkerSymbol}s so
+ * they can be displayed on the MapView.
+ * <p>
+ * For loading a file store on a disk. The application stores a temporary file
+ * onto your machine and then gets the address of where that file was stored.
+ * <h4>How it Works</h4>
+ * 
+ * First a {@link GraphicsOverlay} is created and added to the
+ * {@link MapView#getGraphicsOverlays} method. The PictureMarkerSymbol(String
+ * uri) constructor creates a symbol from a specified URI. This means that a
+ * symbol can be created from a URL, a web page, or one can enter a path
+ * location to a file stored on their machine.
+ * <p>
+ * The new PictureMarkerSymbol(Image) constructor creates a symbol from a Javafx
+ * Image. The Image used for this demo is stored in the resource folder that
+ * comes with the application.
+ * <p>
+ * Lastly once a symbol is created it will need to be added to a {@link Graphic}
+ * along with a Point and then pass that graphic to the
+ * {@link GraphicsOverlay#getGraphics} method.
  */
 public class PictureMarkerSymbolSample extends Application {
 
   private MapView mapView;
   private File orangeSymbolPath;
 
-  private final String CAMPSITE_SYMBOL =
+  private static final String CAMPSITE_SYMBOL =
       "http://sampleserver6.arcgisonline.com/arcgis/rest/services/Recreation/FeatureServer/0/images/e82f744ebb069bb35b234b3fea46deae";
+  private static final String SAMPLES_THEME_PATH =
+      "../resources/SamplesTheme.css";
 
   @Override
   public void start(Stage stage) throws Exception {
 
-    // create border pane and application scene
-    BorderPane borderPane = new BorderPane();
-    Scene scene = new Scene(borderPane);
+    // create stack pane and application scene
+    StackPane stackPane = new StackPane();
+    Scene scene = new Scene(stackPane);
+    scene.getStylesheets().add(getClass().getResource(SAMPLES_THEME_PATH)
+        .toExternalForm());
 
-    // add a title, size the stage, and set scene to stage
-    stage.setTitle("Picture Marker Symbol Example");
-    stage.setHeight(700);
+    // set title, size, and add scene to stage
+    stage.setTitle("Picture Marker Symbol Sample");
     stage.setWidth(800);
+    stage.setHeight(700);
     stage.setScene(scene);
     stage.show();
 
+    // create a control panel
+    VBox vBoxControl = new VBox(6);
+    vBoxControl.setMaxSize(250, 190);
+    vBoxControl.getStyleClass().add("panel-region");
+
+    // create sample description
+    Label descriptionLabel = new Label("Sample Description:");
+    descriptionLabel.getStyleClass().add("panel-label");
+    TextArea description = new TextArea(
+        "This sample shows how to create a Picture Marker Symbol from a URL, locally on"
+            + " the application, or from disk storage");
+    description.setWrapText(true);
+    description.autosize();
+    description.setEditable(false);
+
+    // add label and sample description to the control panel
+    vBoxControl.getChildren().addAll(descriptionLabel, description);
     try {
-      // create points for displaying graphics
-      Point leftPoint = new Point(-228835, 6550763,
-          SpatialReferences.getWebMercator()); // Disk
-      Point rightPoint = new Point(-223560, 6552021,
-          SpatialReferences.getWebMercator()); // URL
-      Point localPoint = new Point(-226773, 6550477,
-          SpatialReferences.getWebMercator());
+
+      // create a map with the topograph basemap
+      final Map map = new Map(Basemap.createTopographic());
 
       // create view for this map
       mapView = new MapView();
 
-      // place map in border pane
-      borderPane.setCenter(mapView);
-
-      // create a map with the topograph basemap
-      Map map = new Map(Basemap.createTopographic());
-
       // create graphics overlay and add it to the mapview
       GraphicsOverlay graphicsOverlay = new GraphicsOverlay();
+
       mapView.getGraphicsOverlays().add(graphicsOverlay);
+
+      // create points for displaying graphics
+      Point leftPoint = new Point(-228835, 6550763, SpatialReferences
+          .getWebMercator()); // Disk
+      Point rightPoint = new Point(-223560, 6552021, SpatialReferences
+          .getWebMercator()); // URL
+      Point middlePoint = new Point(-226773, 6550477, SpatialReferences
+          .getWebMercator());
+
+      // create orange picture marker symbol from disk
+      if (saveResourceToExternalStorage()) {
+        // create orange picture marker symbol
+        PictureMarkerSymbol orangeSymbol = new PictureMarkerSymbol(
+            orangeSymbolPath
+                .getAbsolutePath());
+        // place orange picture marker symbol on map
+        placePictureMarkerSymbol(graphicsOverlay, orangeSymbol, leftPoint);
+      }
+
+      // create blue picture marker symbol from local
+      Image newImage = new Image(getClass()
+          .getResourceAsStream("resources/blue_symbol.png"));
+      PictureMarkerSymbol blueSymbol = new PictureMarkerSymbol(newImage);
+      // place blue picture marker symbol on map
+      placePictureMarkerSymbol(graphicsOverlay, blueSymbol, middlePoint);
 
       // create campsite picture marker symbol from URL
       PictureMarkerSymbol campsiteSymbol = new PictureMarkerSymbol(
@@ -100,45 +155,35 @@ public class PictureMarkerSymbolSample extends Application {
       // place campsite picture marker symbol on map
       placePictureMarkerSymbol(graphicsOverlay, campsiteSymbol, rightPoint);
 
-      // create orange picture marker symbol from disk
-      if (saveResourceToExternalStorage()) {
-        // create orange picture marker symbol
-        PictureMarkerSymbol orangeSymbol = new PictureMarkerSymbol(
-            orangeSymbolPath.getAbsolutePath());
-        // place orange picture marker symbol on map
-        placePictureMarkerSymbol(graphicsOverlay, orangeSymbol, leftPoint);
-      }
-
-      // create blue picture marker symbol from local
-      Image newImage = new Image(
-          getClass().getResourceAsStream("resources/blue_symbol.png"));
-      PictureMarkerSymbol blueSymbol = new PictureMarkerSymbol(newImage);
-      // place blue picture marker symbol on map
-      placePictureMarkerSymbol(graphicsOverlay, blueSymbol, localPoint);
+      campsiteSymbol.addDoneLoadingListener(() -> {
+        Platform.runLater(() -> {
+          stackPane.getChildren().add(vBoxControl);
+          StackPane.setAlignment(vBoxControl, Pos.TOP_LEFT);
+          StackPane.setMargin(vBoxControl, new Insets(10, 0, 0, 10));
+        });
+      });
 
       // set map to be displayed in mapview
       mapView.setMap(map);
 
-      // create initial viewpoint using an envelope
-      Envelope envelope = new Envelope(leftPoint, rightPoint);
-
       // set viewpoint on mapview with padding
+      Envelope envelope = new Envelope(leftPoint, rightPoint);
       mapView.setViewpointGeometryWithPaddingAsync(envelope, 100.0);
 
+      // add the map view and control panel to stack pane
+      stackPane.getChildren().add(mapView);
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
 
   /**
-   * Places a PictureMarkerSymbol, that is 40 by 40, on MapView by adding a
-   * Graphic to the GraphicsOverlay when the PictureMarkerSymbol is done
-   * loading.
+   * Adds a Graphic to the Graphics Overlay using a Point and a Picture Marker
+   * Symbol.
    * 
-   * @param graphicsOverlay used to place the PictureMarkerSymbol onto the
-   *          MapView
-   * @param markerSymbol PictureMarkerSymbol that is to be placed
-   * @param graphicPoint where the PictureMarkerSymbol is going to be placed
+   * @param graphicsOverlay holds information about Graphics
+   * @param markerSymbol PictureMarkerSymbol to be used
+   * @param graphicPoint where the Graphic is going to be placed
    */
   private void placePictureMarkerSymbol(GraphicsOverlay graphicsOverlay,
       PictureMarkerSymbol markerSymbol, Point graphicPoint) {
@@ -147,14 +192,15 @@ public class PictureMarkerSymbolSample extends Application {
     markerSymbol.setHeight(40);
     markerSymbol.setWidth(40);
 
+    // load symbol asynchronously
+    markerSymbol.loadAsync();
+
     // add to the graphic overlay once done loading
     markerSymbol.addDoneLoadingListener(() -> {
       Graphic symbolGraphic = new Graphic(graphicPoint, markerSymbol);
       graphicsOverlay.getGraphics().add(symbolGraphic);
     });
 
-    // load symbol asynchronously
-    markerSymbol.loadAsync();
   }
 
   /**
@@ -169,8 +215,8 @@ public class PictureMarkerSymbolSample extends Application {
       orangeSymbolPath = File.createTempFile("orange_symbol", ".png");
 
       // save image from resources folder to computer's disk
-      Image orangeImage = new Image(
-          getClass().getResourceAsStream("resources/orange_symbol.png"));
+      Image orangeImage = new Image(getClass().getResourceAsStream(
+          "resources/orange_symbol.png"));
 
       ImageIO.write(SwingFXUtils.fromFXImage(orangeImage, null), "png",
           orangeSymbolPath.getAbsoluteFile());
@@ -209,4 +255,5 @@ public class PictureMarkerSymbolSample extends Application {
 
     Application.launch(args);
   }
+
 }
