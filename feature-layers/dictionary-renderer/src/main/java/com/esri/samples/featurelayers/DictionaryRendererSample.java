e34b1385ba933610979f0b14a95973839944e5c6
JC <JC@10.112.23.130>
2016-07-06 17:23:24 +0100

    needs to be reviewed

--- feature-layers/dictionary-renderer/src/main/java/com/esri/samples/featurelayers/DictionaryRendererSample.java
+++ feature-layers/dictionary-renderer/src/main/java/com/esri/samples/featurelayers/DictionaryRendererSample.java
@@ -15,13 +15,33 @@
  */
 package com.esri.samples.featurelayers;
 
+import java.io.File;
+
+import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import com.esri.arcgisruntime.datasource.arcgis.Geodatabase;
+import com.esri.arcgisruntime.geometry.Envelope;
+import com.esri.arcgisruntime.geometry.GeometryEngine;
+import com.esri.arcgisruntime.layers.FeatureLayer;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.LayerList;
+import com.esri.arcgisruntime.mapping.Viewpoint;
+import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.symbology.DictionaryRenderer;
+import com.esri.arcgisruntime.symbology.SymbolDictionary;
+
 public class DictionaryRendererSample extends Application {
 
+  // number of layers added to the map
+  private int layerCounter = 0;
+
   private MapView mapView;
+  // location for view to zoom to after layers are loaded to map
+  private Envelope initialExtent;
 
   @Override
   public void start(Stage stage) throws Exception {
@@ -35,6 +55,43 @@ public class DictionaryRendererSample extends Application {
     stage.setHeight(700);
     stage.setScene(scene);
     stage.show();
+
+    ArcGISMap map = new ArcGISMap(Basemap.createTopographic());
+    mapView.setMap(map);
+
+    // load geo-database from local location
+    File databaseLocation = new File(getClass().getResource("/militaryoverlay.geodatabase").getPath());
+    Geodatabase geodatabase = new Geodatabase(databaseLocation.getAbsolutePath());
+    geodatabase.loadAsync();
+
+    geodatabase.addDoneLoadingListener(() -> {
+      SymbolDictionary symbolDictionary = new SymbolDictionary("mil2525d");
+
+      geodatabase.getGeodatabaseFeatureTables().forEach(table -> {
+        FeatureLayer featureLayer = new FeatureLayer(table);
+        featureLayer.loadAsync();
+
+        featureLayer.addDoneLoadingListener(() -> {
+          layerCounter++;
+          if (layerCounter == geodatabase.getGeodatabaseFeatureTables().size()) {
+            LayerList mapLayers = map.getOperationalLayers();
+            if (mapLayers.size() >= 0) {
+              initialExtent = null;
+              mapLayers.forEach(layer -> {
+                initialExtent = initialExtent == null ? layer.getFullExtent()
+                    : GeometryEngine.union(initialExtent, layer.getFullExtent()).getExtent();
+              });
+              mapView.setViewpoint(new Viewpoint(initialExtent));
+            }
+          }
+        });
+
+        map.getOperationalLayers().add(featureLayer);
+        // render tells layer what symbols to apply to what features
+        DictionaryRenderer dictionaryRenderer = new DictionaryRenderer(symbolDictionary);
+        featureLayer.setRenderer(dictionaryRenderer);
+      });
+    });
   }
 
   /**
