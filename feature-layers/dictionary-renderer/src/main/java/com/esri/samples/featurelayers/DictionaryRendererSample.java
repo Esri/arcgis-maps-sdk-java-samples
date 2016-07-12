f4e7e8466f8f24833aa00b16df12a0a2d5a95545
Tyler Schiewe <tschiewe@esri.com>
2016-07-12 15:44:08 -0700

    simplified code slightly

--- feature-layers/dictionary-renderer/src/main/java/com/esri/samples/featurelayers/DictionaryRendererSample.java
+++ feature-layers/dictionary-renderer/src/main/java/com/esri/samples/featurelayers/DictionaryRendererSample.java
@@ -1,18 +1,14 @@
 /*
- * Copyright 2015 Esri.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * Copyright 2015 Esri. Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
+ * or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
  */
+
 package com.esri.samples.featurelayers;
 
 import java.io.File;
@@ -28,23 +24,18 @@ import com.esri.arcgisruntime.geometry.GeometryEngine;
 import com.esri.arcgisruntime.layers.FeatureLayer;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
-import com.esri.arcgisruntime.mapping.LayerList;
-import com.esri.arcgisruntime.mapping.Viewpoint;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.symbology.DictionaryRenderer;
 import com.esri.arcgisruntime.symbology.SymbolDictionary;
 
 public class DictionaryRendererSample extends Application {
 
-  // number of layers added to the map
-  private int layerCounter = 0;
-
   private MapView mapView;
-  // location for view to zoom to after layers are loaded to map
-  private Envelope initialExtent;
+  private Envelope extent;
 
   @Override
   public void start(Stage stage) throws Exception {
+
     mapView = new MapView();
     StackPane appWindow = new StackPane(mapView);
     Scene scene = new Scene(appWindow);
@@ -68,31 +59,18 @@ public class DictionaryRendererSample extends Application {
     SymbolDictionary symbolDictionary = new SymbolDictionary("mil2525d");
     symbolDictionary.loadAsync();
 
-    geodatabase.addDoneLoadingListener(() -> {
-      geodatabase.getGeodatabaseFeatureTables().forEach(table -> {
-        FeatureLayer featureLayer = new FeatureLayer(table);
-        featureLayer.loadAsync();
-
-        featureLayer.addDoneLoadingListener(() -> {
-          layerCounter++;
-          if (layerCounter == geodatabase.getGeodatabaseFeatureTables().size()) {
-            LayerList mapLayers = map.getOperationalLayers();
-            if (mapLayers.size() >= 0) {
-              initialExtent = null;
-              mapLayers.forEach(layer -> {
-                initialExtent = initialExtent == null ? layer.getFullExtent()
-                    : GeometryEngine.union(initialExtent, layer.getFullExtent()).getExtent();
-              });
-              mapView.setViewpoint(new Viewpoint(initialExtent));
-            }
-          }
-        });
-
-        map.getOperationalLayers().add(featureLayer);
-        DictionaryRenderer dictionaryRenderer = new DictionaryRenderer(symbolDictionary);
-        featureLayer.setRenderer(dictionaryRenderer);
+    geodatabase.addDoneLoadingListener(() -> geodatabase.getGeodatabaseFeatureTables().forEach(table -> {
+      FeatureLayer featureLayer = new FeatureLayer(table);
+      map.getOperationalLayers().add(featureLayer);
+      DictionaryRenderer dictionaryRenderer = new DictionaryRenderer(symbolDictionary);
+      featureLayer.setRenderer(dictionaryRenderer);
+      featureLayer.loadAsync();
+      featureLayer.addDoneLoadingListener(() -> {
+        extent = extent == null ? featureLayer.getFullExtent() : GeometryEngine.union(extent, featureLayer
+            .getFullExtent()).getExtent();
+        mapView.setViewpointGeometryAsync(extent);
       });
-    });
+    }));
   }
 
   /**
