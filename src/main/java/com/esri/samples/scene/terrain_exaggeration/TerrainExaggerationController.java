3cd245e18bf174ff97d655b4e3f90b9115a0c7eb
JC <JC@10.112.23.130>
2016-12-21 12:25:31 +0000

    Minor change

--- src/main/java/com/esri/samples/scene/terrain_exaggeration/TerrainExaggerationController.java
+++ src/main/java/com/esri/samples/scene/terrain_exaggeration/TerrainExaggerationController.java
@@ -13,6 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
+
 package com.esri.samples.scene.terrain_exaggeration;
 
 import javafx.fxml.FXML;
