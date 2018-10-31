26f20f285e731d7405e49afdac396fe87aeb9715
Rachael Ellen <rellen@esri.com>
2018-10-31 14:30:59 +0000

    Updates to ReadMe
    Added README file, image of sample and image of scale bar types, and starter code for ScaleBar

---
+++ src/main/java/com/esri/samples/mapview/scale_bar/ScaleBarSample.java
@@ -0,0 +1,11 @@
+package com.esri.samples.mapview.scale_bar;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ScaleBarSample extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
