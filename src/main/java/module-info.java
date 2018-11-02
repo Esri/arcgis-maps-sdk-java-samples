75dc3a5f574e6ee37676ea02504a43bf847769c3
Tyler Schiewe <tyler.schiewe@gmail.com>
2018-11-02 10:55:32 -0700

    start updates for java 11

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.esri.samples {
+  requires transitive java.sql;
+  requires transitive javafx.graphics;
+  requires transitive javafx.controls;
+  requires transitive com.esri.arcgisruntime;
+  requires transitive javafx.fxml;
+  requires transitive javafx.swing;
+  requires transitive javafx.web;
+}
