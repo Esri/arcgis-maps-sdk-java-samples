272bde42bf821876d2e97885caaa17a8884eacf3
Rachael Ellen <rellen@esri.com>
2018-11-12 15:18:46 +0000

    Update src/main/java/com/esri/samples/geometry/convex_hull_list/ConvexHullListSample.java
    Hard coded polygons for displaying on map.

--- src/main/java/com/esri/samples/geometry/convex_hull_list/ConvexHullListSample.java
+++ src/main/java/com/esri/samples/geometry/convex_hull_list/ConvexHullListSample.java
@@ -16,17 +16,27 @@
 
 package com.esri.samples.geometry.convex_hull_list;
 
+import com.esri.arcgisruntime.geometry.PolygonBuilder;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.Graphic;
 import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.symbology.SimpleFillSymbol;
+import com.esri.arcgisruntime.symbology.SimpleLineSymbol;
+import com.esri.arcgisruntime.symbology.SimpleRenderer;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.scene.layout.*;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
@@ -35,6 +45,7 @@ import javafx.scene.control.Button;
 public class ConvexHullListSample extends Application {
 
   private MapView mapView;
+  private GraphicsOverlay graphicsOverlay;
 
   @Override
   public void start(Stage stage) {
@@ -56,11 +67,66 @@ public class ConvexHullListSample extends Application {
       mapView = new MapView();
       mapView.setMap(map);
 
-      // create a graphics overlay for displaying point graphic and convex hull
-      GraphicsOverlay pointGraphicOverlay = new GraphicsOverlay();
-      mapView.getGraphicsOverlays().add(pointGraphicOverlay);
+      // create a graphics overlay for displaying polygon graphic and convex hull
+      graphicsOverlay = new GraphicsOverlay();
 
-      // create a graphic to show the polygons with blue outlines and blue transparent fill
+      // create a simple line symbol for the outline of the two input polygon graphics
+      SimpleLineSymbol polygonOutline = new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, 0xFF0000FF, 3);
+      // create a simple fill symbol for the two input polygon graphics
+      SimpleFillSymbol polygonFill = new SimpleFillSymbol(SimpleFillSymbol.Style.SOLID, 0x300000FF, polygonOutline);
+
+      // create the first polygon graphic, and add to the map view
+      GraphicsOverlay firstPolygonGraphicOverlay = new GraphicsOverlay();
+      PolygonBuilder firstPolygonGeometry = new PolygonBuilder(SpatialReferences.getWebMercator());
+      firstPolygonGeometry.addPoint(-4983189.15470412, 8679428.55774286);
+      firstPolygonGeometry.addPoint(-5222621.66664186, 5147799.00666126);
+      firstPolygonGeometry.addPoint(-13483043.3284937, 4728792.11077023);
+      firstPolygonGeometry.addPoint(-13273539.8805482, 2244679.79941622);
+      firstPolygonGeometry.addPoint(-5372266.98660294, 2035176.3514707);
+      firstPolygonGeometry.addPoint(-5432125.11458738, -4100281.76693377);
+      firstPolygonGeometry.addPoint(-2469147.7793579, -4160139.89491821);
+      firstPolygonGeometry.addPoint(-1900495.56350578, 2035176.3514707);
+      firstPolygonGeometry.addPoint(2768438.41928007, 1975318.22348627);
+      firstPolygonGeometry.addPoint(2409289.65137346, 5477018.71057565);
+      firstPolygonGeometry.addPoint(-2409289.65137346, 5387231.518599);
+      firstPolygonGeometry.addPoint(-2469147.7793579, 8709357.62173508);
+      Graphic firstPolygonGraphic = new Graphic(firstPolygonGeometry.toGeometry());
+      // set the Z index for the first polygon graphic so that it appears above the convex hull graphic added later
+      firstPolygonGraphic.setZIndex(1);
+      firstPolygonGraphicOverlay.getGraphics().add(firstPolygonGraphic);
+
+      // render the first polygon
+      SimpleRenderer firstPolygonRenderer = new SimpleRenderer(polygonFill);
+      firstPolygonGraphicOverlay.setRenderer(firstPolygonRenderer);
+      // add first polygon to the map view
+      mapView.getGraphicsOverlays().add(firstPolygonGraphicOverlay);
+
+      // create the second polygon graphic, and add to the map
+      GraphicsOverlay secondPolygonGraphicOverlay = new GraphicsOverlay();
+      PolygonBuilder secondPolygonGeometry = new PolygonBuilder(SpatialReferences.getWebMercator());
+      secondPolygonGeometry.addPoint(5993520.19456882, -1063938.49607736);
+      secondPolygonGeometry.addPoint(3085421.63862418, -1383120.04490055);
+      secondPolygonGeometry.addPoint(3794713.96934239, -2979027.78901651);
+      secondPolygonGeometry.addPoint(6880135.60796657, -4078430.90162972);
+      secondPolygonGeometry.addPoint(7092923.30718203, -2837169.32287287);
+      secondPolygonGeometry.addPoint(8617901.81822617, -2092412.37561875);
+      secondPolygonGeometry.addPoint(6986529.4575743, 354646.16535905);
+      secondPolygonGeometry.addPoint(5319692.48038653, 1205796.96222089);
+      Graphic secondPolygonGraphic = new Graphic(secondPolygonGeometry.toGeometry());
+      // set the Z index for the second polygon graphic so that it appears above the convex hull graphic added later
+      secondPolygonGraphic.setZIndex(1);
+      secondPolygonGraphicOverlay.getGraphics().add(secondPolygonGraphic);
+
+      // render the second polygon
+      SimpleRenderer secondPolygonRenderer = new SimpleRenderer(polygonFill);
+      secondPolygonGraphicOverlay.setRenderer(secondPolygonRenderer);
+      mapView.getGraphicsOverlays().add(secondPolygonGraphicOverlay);
+
+
+      // Set the Z index for the polygon1 graphic so that it appears above the convex hull graphic(s) added later.
+
+
+
 
       // create a graphic to show the convex hull as a red outline
 
@@ -85,7 +151,6 @@ public class ConvexHullListSample extends Application {
       controlsVBox.setPadding(new Insets(10));
       controlsVBox.setMaxSize(260, 110);
       controlsVBox.getStyleClass().add("panel-region");
-
       controlsVBox.getChildren().addAll(informationLabel, convexHullButton, clearButton, checkBox);
 
 
@@ -101,6 +166,8 @@ public class ConvexHullListSample extends Application {
       e.printStackTrace();
     }
   }
+
+
 
   /**
    * Stops and releases all resources used in application.
