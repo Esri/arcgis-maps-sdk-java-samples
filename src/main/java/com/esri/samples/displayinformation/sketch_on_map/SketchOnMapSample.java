e30ea77cbd02348a5c5a4dd3c57b614280be7635
Rachael Ellen <rellen@esri.com>
2018-11-21 17:55:57 +0000

    Update SketchOnMapSample.java
    added buttons for controls

--- src/main/java/com/esri/samples/displayinformation/sketch_on_map/SketchOnMapSample.java
+++ src/main/java/com/esri/samples/displayinformation/sketch_on_map/SketchOnMapSample.java
@@ -17,6 +17,7 @@
 package com.esri.samples.displayinformation.sketch_on_map;
 
 import com.esri.arcgisruntime.geometry.Geometry;
+import com.esri.arcgisruntime.geometry.GeometryType;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.view.Graphic;
@@ -24,6 +25,9 @@ import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.mapping.view.SketchCreationMode;
 import com.esri.arcgisruntime.mapping.view.SketchEditor;
+import com.esri.arcgisruntime.symbology.SimpleFillSymbol;
+import com.esri.arcgisruntime.symbology.SimpleLineSymbol;
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -46,12 +50,15 @@ public class SketchOnMapSample extends Application {
 
   private MapView mapView;
   private SketchEditor sketchEditor;
-  private Button redoButton;
-  private Button undoButton;
-  private Button clearButton;
-  private Button doneButton;
-  private Button editButton;
+//  private Button redoButton;
+//  private Button undoButton;
+//  private Button clearButton;
+//  private Button doneButton;
+//  private Button editButton;
   private GraphicsOverlay graphicsOverlay;
+  private SimpleFillSymbol fillSymbol;
+  private SimpleLineSymbol lineSymbol;
+  private SimpleMarkerSymbol pointSymbol;
 
   @Override
   public void start(Stage stage) {
@@ -84,9 +91,57 @@ public class SketchOnMapSample extends Application {
       sketchEditor = new SketchEditor();
       mapView.setSketchEditor(sketchEditor);
 
+      // define symbols
+      pointSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.SQUARE, 0xFFFF0000,20);
+      lineSymbol = new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, 0xFFFF8800, 4);
+      fillSymbol = new SimpleFillSymbol(SimpleFillSymbol.Style.CROSS, 0x40FFA9A9, lineSymbol);
+
       // create a combo box for graphic options
       ComboBox<SketchCreationMode> sketchOptionsDropDown = new ComboBox<>();
       sketchOptionsDropDown.getItems().setAll(SketchCreationMode.values());
+      sketchOptionsDropDown.setMaxWidth(Double.MAX_VALUE);
+
+      // create buttons for user interaction
+      Button undoButton            = new Button("Undo changes to sketch");
+      Button redoButton            = new Button("Redo changes to sketch");
+      Button clearButton           = new Button("Clear graphics overlay");
+      Button doneButton            = new Button ("Save sketch to graphics overlay");
+      Button editButton            = new Button("Edit sketch");
+
+      undoButton.setMaxWidth(Double.MAX_VALUE);
+      redoButton.setMaxWidth(Double.MAX_VALUE);
+      clearButton.setMaxWidth(Double.MAX_VALUE);
+      doneButton.setMaxWidth(Double.MAX_VALUE);
+      editButton.setMaxWidth(Double.MAX_VALUE);
+
+      // disable clear, undo, redo and edit buttons when starting application
+      clearButton.setDisable(true);
+      redoButton.setDisable(true);
+      undoButton.setDisable(true);
+      editButton.setDisable(true);
+      sketchOptionsDropDown.setPromptText("    -- Select a graphic to sketch --");
+
+      // save the sketch as a graphic, and store graphic to the graphics overlay
+      doneButton.setOnAction(event -> {
+        storeGraphic();
+        clearButton.setDisable(false);
+      });
+
+      // clear the graphics overlay, and disable the clear button
+      clearButton.setOnAction(event -> {
+        graphicsOverlay.getGraphics().clear();
+        clearButton.setDisable(true);
+        redoButton.setDisable(true);
+        undoButton.setDisable(true);
+        editButton.setDisable(true);
+      });
+
+      // undo the last change made whilst sketching graphic
+      undoButton.setOnAction(event -> {
+        if (sketchEditor.canUndo()) {
+          sketchEditor.undo();
+        };
+      });
 
       sketchOptionsDropDown.getSelectionModel().selectedItemProperty().addListener(o -> {
 
@@ -121,34 +176,12 @@ public class SketchOnMapSample extends Application {
 
       });
 
-
-
-
-      // create buttons for user interaction
-      undoButton            = new Button("Undo");
-      redoButton            = new Button("Redo");
-      clearButton           = new Button("Clear");
-      doneButton            = new Button ("Done, save");
-      editButton            = new Button("Edit");
-
-      doneButton.setOnAction(event -> {
-
-      });
-
-
-//      polylineButton.setOnAction(event -> {
-//        this.createPolyline();
-//      });
-
-
-
-
       // create a control panel
       VBox controlsVBox = new VBox(6);
       controlsVBox.setBackground(new Background(new BackgroundFill(Paint.valueOf("rgba(0, 0, 0, 0.3)"),
               CornerRadii.EMPTY, Insets.EMPTY)));
       controlsVBox.setPadding(new Insets(10));
-      controlsVBox.setMaxSize(140, 110);
+      controlsVBox.setMaxSize(240, 110);
       controlsVBox.getStyleClass().add("panel-region");
       controlsVBox.getChildren().addAll(sketchOptionsDropDown, undoButton, redoButton, clearButton, doneButton, editButton);
 
@@ -156,7 +189,6 @@ public class SketchOnMapSample extends Application {
       stackPane.getChildren().addAll(mapView, controlsVBox);
       stackPane.setAlignment(controlsVBox, Pos.TOP_RIGHT);
       stackPane.setMargin(controlsVBox, new Insets(10, 10, 0, 10));
-
 
 
     } catch (Exception e) {
@@ -207,21 +239,22 @@ public class SketchOnMapSample extends Application {
       // create a graphic from the sketch editor geometry
       Graphic graphic = new Graphic(sketchGeometry);
 
+      // assign a symbol based on geometry type
+      if (graphic.getGeometry().getGeometryType() == GeometryType.POLYGON) {
+        graphic.setSymbol(fillSymbol);
+      } else if (graphic.getGeometry().getGeometryType() == GeometryType.POLYLINE) {
+        graphic.setSymbol(lineSymbol);
+      } else if (graphic.getGeometry().getGeometryType() == GeometryType.POINT ||
+          graphic.getGeometry().getGeometryType() == GeometryType.MULTIPOINT) {
+        graphic.setSymbol(pointSymbol);
+      }
+
       // add the graphic to the graphics overlay
       graphicsOverlay.getGraphics().add(graphic);
+      undoButton.setDisable(false);
+
     }
   }
-
-
-
-//  private void resetButtons() {
-//    polylineButton.setDisable(true);
-//    polygonButton.setDisable(true);
-//    pointButton.setDisable(true);
-//    multiPointButton.setDisable(true);
-//    freeHandLineButton.setDisable(true);
-//    freeHandPolygonButton.setDisable(true);
-//  }
 
 
 
