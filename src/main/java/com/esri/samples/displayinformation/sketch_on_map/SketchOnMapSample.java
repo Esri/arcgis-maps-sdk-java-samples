819f4b0f4d32e69625f56c5014d4c96f03d31876
Rachael Ellen <rellen@esri.com>
2018-11-21 12:42:59 +0000

    Create SketchOnMapSample.java
    Initialising sketch on map sample

---
+++ src/main/java/com/esri/samples/displayinformation/sketch_on_map/SketchOnMapSample.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2018 Esri.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.esri.samples.displayinformation.sketch_on_map;
+
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
+import com.esri.arcgisruntime.mapping.view.MapView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class SketchOnMapSample extends Application {
+
+  private MapView mapView;
+
+  @Override
+  public void start(Stage stage) {
+
+    try {
+      // create stack pane and application scene
+      StackPane stackPane = new StackPane();
+      Scene scene = new Scene(stackPane);
+      scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+
+      // set title, size, and add scene to stage
+      stage.setTitle("Sketch on Map Sample");
+      stage.setWidth(1000);
+      stage.setHeight(700);
+      stage.setScene(scene);
+      stage.show();
+
+      // create a map with a basemap and add it to the map view
+      ArcGISMap map = new ArcGISMap(Basemap.Type.IMAGERY, 64.3286, -15.5314, 13 );
+      mapView = new MapView();
+      mapView.setMap(map);
+
+      // create a graphics overlay for the graphics
+      GraphicsOverlay graphicsOverlay = new GraphicsOverlay();
+
+      // add the graphics overlay to the map view
+      mapView.getGraphicsOverlays().add(graphicsOverlay);
+
+      // create a new sketch editor and add it to the map view
+
+
+
+
+      // add the map view to the stack pane
+      stackPane.getChildren().addAll(mapView);
+
+
+
+    } catch (Exception e) {
+
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Stops and releases all resources used in application
+   */
+  @Override
+  public void stop() {
+    // release resources when the application closes
+    if (mapView != null) {
+      mapView.dispose();
+    }
+  }
+
+  /**
+   * Opens and runs application.
+   *
+   * @param args arguments passed to this application
+   */
+  public static void main(String[] args) {
+
+    Application.launch(args);
+  }
+}
