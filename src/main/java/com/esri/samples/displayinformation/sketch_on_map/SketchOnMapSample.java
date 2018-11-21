dc6d27aa0248cc32f7221442f4ae674ec77888bc
Rachael Ellen <rellen@esri.com>
2018-11-21 16:41:53 +0000

    Update SketchOnMapSample.java
    Adding combobox for graphic selection

--- src/main/java/com/esri/samples/displayinformation/sketch_on_map/SketchOnMapSample.java
+++ src/main/java/com/esri/samples/displayinformation/sketch_on_map/SketchOnMapSample.java
@@ -16,18 +16,42 @@
 
 package com.esri.samples.displayinformation.sketch_on_map;
 
+import com.esri.arcgisruntime.geometry.Geometry;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.Graphic;
 import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.mapping.view.SketchCreationMode;
+import com.esri.arcgisruntime.mapping.view.SketchEditor;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SketchOnMapSample extends Application {
 
   private MapView mapView;
+  private SketchEditor sketchEditor;
+  private Button redoButton;
+  private Button undoButton;
+  private Button clearButton;
+  private Button doneButton;
+  private Button editButton;
+  private GraphicsOverlay graphicsOverlay;
 
   @Override
   public void start(Stage stage) {
@@ -51,18 +75,87 @@ public class SketchOnMapSample extends Application {
       mapView.setMap(map);
 
       // create a graphics overlay for the graphics
-      GraphicsOverlay graphicsOverlay = new GraphicsOverlay();
+      graphicsOverlay = new GraphicsOverlay();
 
       // add the graphics overlay to the map view
       mapView.getGraphicsOverlays().add(graphicsOverlay);
 
       // create a new sketch editor and add it to the map view
+      sketchEditor = new SketchEditor();
+      mapView.setSketchEditor(sketchEditor);
+
+      // create a combo box for graphic options
+      ComboBox<SketchCreationMode> sketchOptionsDropDown = new ComboBox<>();
+      sketchOptionsDropDown.getItems().setAll(SketchCreationMode.values());
+
+      sketchOptionsDropDown.getSelectionModel().selectedItemProperty().addListener(o -> {
+
+        // create a graphics draw option based on source type
+        SketchCreationMode sketchCreationMode = sketchOptionsDropDown.getSelectionModel().getSelectedItem();
+
+        try {
+          switch (sketchCreationMode) {
+            case POLYLINE:
+              createPolyline();
+              break;
+            case POLYGON:
+              createPolygon();
+              break;
+            case POINT:
+              createPoint();
+              break;
+            case MULTIPOINT:
+              createMultiPoint();
+              break;
+            case FREEHAND_LINE:
+              createFreeHandLine();
+              break;
+            case FREEHAND_POLYGON:
+              createFreeHandPolygon();
+              break;
+          }
+
+        } catch (Exception e) {
+          new Alert(Alert.AlertType.ERROR, "Error finding draw style").show();
+        }
+
+      });
 
 
 
+
+      // create buttons for user interaction
+      undoButton            = new Button("Undo");
+      redoButton            = new Button("Redo");
+      clearButton           = new Button("Clear");
+      doneButton            = new Button ("Done, save");
+      editButton            = new Button("Edit");
+
+      doneButton.setOnAction(event -> {
+
+      });
+
+
+//      polylineButton.setOnAction(event -> {
+//        this.createPolyline();
+//      });
+
+
+
+
+      // create a control panel
+      VBox controlsVBox = new VBox(6);
+      controlsVBox.setBackground(new Background(new BackgroundFill(Paint.valueOf("rgba(0, 0, 0, 0.3)"),
+              CornerRadii.EMPTY, Insets.EMPTY)));
+      controlsVBox.setPadding(new Insets(10));
+      controlsVBox.setMaxSize(140, 110);
+      controlsVBox.getStyleClass().add("panel-region");
+      controlsVBox.getChildren().addAll(sketchOptionsDropDown, undoButton, redoButton, clearButton, doneButton, editButton);
 
       // add the map view to the stack pane
-      stackPane.getChildren().addAll(mapView);
+      stackPane.getChildren().addAll(mapView, controlsVBox);
+      stackPane.setAlignment(controlsVBox, Pos.TOP_RIGHT);
+      stackPane.setMargin(controlsVBox, new Insets(10, 10, 0, 10));
 
 
 
@@ -71,6 +164,66 @@ public class SketchOnMapSample extends Application {
       e.printStackTrace();
     }
   }
+
+  private void createPolyline() {
+    sketchEditor.start(SketchCreationMode.POLYLINE);
+  }
+
+  private void createPolygon() { sketchEditor.start(SketchCreationMode.POLYGON);
+  }
+
+  private void createPoint() {
+    sketchEditor.start(SketchCreationMode.POINT);
+  }
+
+  private void createMultiPoint() {
+    sketchEditor.start(SketchCreationMode.MULTIPOINT);
+  }
+
+  private void createFreeHandLine() {
+    sketchEditor.start(SketchCreationMode.FREEHAND_LINE);
+  }
+
+  private void createFreeHandPolygon() {
+    sketchEditor.start(SketchCreationMode.FREEHAND_POLYGON);
+  }
+
+  /**
+   * When the done button is clicked, check that sketch is valid. If so, get the geometry from the sketch, set its
+   * symbol and add it to the graphics overlay.
+   */
+  private void storeGraphic() {
+    if (!sketchEditor.isSketchValid()) {
+      sketchEditor.stop();
+      return;
+    }
+
+    // get the geometry from sketch editor
+    Geometry sketchGeometry = sketchEditor.getGeometry();
+    sketchEditor.stop();
+
+    if (sketchGeometry != null) {
+
+      // create a graphic from the sketch editor geometry
+      Graphic graphic = new Graphic(sketchGeometry);
+
+      // add the graphic to the graphics overlay
+      graphicsOverlay.getGraphics().add(graphic);
+    }
+  }
+
+
+
+//  private void resetButtons() {
+//    polylineButton.setDisable(true);
+//    polygonButton.setDisable(true);
+//    pointButton.setDisable(true);
+//    multiPointButton.setDisable(true);
+//    freeHandLineButton.setDisable(true);
+//    freeHandPolygonButton.setDisable(true);
+//  }
+
+
 
   /**
    * Stops and releases all resources used in application
