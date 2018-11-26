2ce991faaf3fe520a18774b62402f6bd4cae4162
Tyler Schiewe <tyler.schiewe@gmail.com>
2018-11-26 14:30:37 -0800

    Delete module-info.java

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.esri.samples {
-  requires transitive java.sql;
-  requires transitive javafx.graphics;
-  requires transitive javafx.controls;
-  requires transitive com.esri.arcgisruntime;
-  requires transitive javafx.fxml;
-  requires transitive javafx.swing;
-  requires transitive javafx.web;
-}
