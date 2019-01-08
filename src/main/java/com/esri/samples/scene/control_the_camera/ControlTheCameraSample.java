4584afa8a1b666ea0be5b7540601d3a8499e9420
Rachael Ellen <rellen@esri.com>
2019-01-08 17:36:12 +0000

    Create java and README files

---
+++ src/main/java/com/esri/samples/scene/control_the_camera/ControlTheCameraSample.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2019 Esri.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.esri.samples.scene.control_the_camera;
+
+import com.esri.arcgisruntime.mapping.ArcGISScene;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.SceneView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class ControlTheCameraSample extends Application {
+
+  private SceneView sceneView;
+
+  @Override
+  public void start(Stage stage) {
+
+    try {
+      // create stack pane and JavaFX app scene
+      StackPane stackPane = new StackPane();
+      Scene fxScene = new Scene(stackPane);
+
+      // set title, size, and add JavaFX scene to stage
+      stage.setTitle("Control the camera");
+      stage.setWidth(800);
+      stage.setHeight(700);
+      stage.setScene(fxScene);
+      stage.show();
+
+      // create a scene and add a basemap to it
+      ArcGISScene scene = new ArcGISScene();
+      scene.setBasemap(Basemap.createImagery());
+
+      sceneView = new SceneView();
+      sceneView.setArcGISScene(scene);
+
+      
+
+
+      // add scene view to the stack pane
+      stackPane.getChildren().addAll(sceneView);
+
+
+
+    } catch (Exception e) {
+      // on any error, display the stack trace.
+      e.printStackTrace();
+    }
+
+
+  }
+
+  /**
+   * Stops and releases all resources used in application.
+   */
+  @Override
+  public void stop() {
+    // release resources when the application closes
+    if (sceneView != null) {
+      sceneView.dispose();
+    }
+  }
+
+  /**
+   * Opens and runs application.
+   *
+   * @param args arguments passed to this application
+   */
+  public static void main(String[] args) {
+
+    Application.launch(args);
+  }
+
+}
---
+++ src/main/java/com/esri/samples/scene/control_the_camera/README.md
@@ -0,0 +1,38 @@
+<h1>Control the Camera</h1>
+
+<p>Use the camera controls to view the scene.</p>
+
+<p><img src="ControlTheCamera.png"/></p>
+
+<h2>How to use the sample</h2>
+
+<p></p>
+
+<h2>How it works</h2>
+
+<p></p>
+
+<ol>
+  <li></li>
+  <li></li>
+</ol>
+
+<h2>Relevant API</h2>
+
+<ul>
+  <li>3D</li>
+  <li>ArcGISScene</li>
+  <li>Camera</li>
+  <li>GlobeCameraController</li>
+  <li>Graphic</li>
+  <li>GraphicsOverlay</li>
+  <li>LayerSceneProperties.SurfacePlacement</li>
+  <li>ModelSceneSymbol</li>
+  <li>OrbitGeoElementCameraController</li>
+  <li>Renderer</li>
+  <li>Renderer.SceneProperties</li>
+  <li>SceneView</li>
+  <li>Viewpoint</li>
+</ul>
+
+
