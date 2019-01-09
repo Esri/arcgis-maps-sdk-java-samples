188a17debad489d69ee53e3a91934467ee8b250d
Rachael Ellen <rellen@esri.com>
2019-01-09 17:58:07 +0000

    Update ControlTheCameraSample.java

--- src/main/java/com/esri/samples/scene/control_the_camera/ControlTheCameraSample.java
+++ src/main/java/com/esri/samples/scene/control_the_camera/ControlTheCameraSample.java
@@ -16,20 +16,51 @@
 
 package com.esri.samples.scene.control_the_camera;
 
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.geometry.SpatialReference;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
 import com.esri.arcgisruntime.mapping.ArcGISScene;
 import com.esri.arcgisruntime.mapping.ArcGISTiledElevationSource;
 import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.Surface;
+
+import com.esri.arcgisruntime.mapping.Viewpoint;
 import com.esri.arcgisruntime.mapping.view.Camera;
+import com.esri.arcgisruntime.mapping.view.CameraController;
+import com.esri.arcgisruntime.mapping.view.GlobeCameraController;
+import com.esri.arcgisruntime.mapping.view.Graphic;
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
+import com.esri.arcgisruntime.mapping.view.LayerSceneProperties;
+import com.esri.arcgisruntime.mapping.view.OrbitGeoElementCameraController;
+import com.esri.arcgisruntime.mapping.view.OrbitLocationCameraController;
 import com.esri.arcgisruntime.mapping.view.SceneView;
+import com.esri.arcgisruntime.symbology.ModelSceneSymbol;
+
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+
+import java.beans.EventHandler;
+import java.io.File;
 
 public class ControlTheCameraSample extends Application {
 
+  private OrbitGeoElementCameraController orbitCameraController;
   private SceneView sceneView;
+  private Graphic plane3D;
+  private static final SpatialReference WGS84 = SpatialReferences.getWgs84();
+
 
   @Override
   public void start(Stage stage) {
@@ -60,16 +91,78 @@ public class ControlTheCameraSample extends Application {
       surface.getElevationSources().add(elevationSource);
       scene.setBaseSurface(surface);
 
-      // add a camera
+      // create a control panel
 
-      Camera camera = new Camera(64.416919, -14.483728, 100, 318, 105, 0);
-      sceneView.setViewpointCamera(camera);
+      VBox controlsVBox = new VBox(6);
+      controlsVBox.setBackground(new Background(new BackgroundFill(Paint.valueOf("rgba(0, 0, 0, 0.3)"),
+              CornerRadii.EMPTY, Insets.EMPTY)));
+      controlsVBox.setPadding(new Insets(10.0));
+      controlsVBox.setMaxSize(260, 110);
+      controlsVBox.getStyleClass().add("panel-region");
+
+      Button activateCamera = new Button("Fix camera to object");
+      Button activateGlobalView = new Button ("Free pan");
+
+      Circle circle = new Circle(25);
+      controlsVBox.getChildren().addAll(circle, activateCamera, activateGlobalView);
+
+
+      // create a graphics overlay for the scene
+      GraphicsOverlay sceneGraphicsOverlay = new GraphicsOverlay();
+      sceneGraphicsOverlay.getSceneProperties().setSurfacePlacement(LayerSceneProperties.SurfacePlacement.ABSOLUTE);
+      sceneView.getGraphicsOverlays().add(sceneGraphicsOverlay);
+
+      // create a graphic with a ModelSceneSymbol of a plane to add to the scene
+      String modelURI = new File("./samples-data/bristol/Collada/Bristol.dae").getAbsolutePath();
+      ModelSceneSymbol plane3DSymbol = new ModelSceneSymbol(modelURI, 1.0);
+      plane3DSymbol.loadAsync();
+      plane3DSymbol.setHeading(45);
+      plane3D = new Graphic(new Point(6.637, 45.399, 1955, WGS84), plane3DSymbol);
+      sceneGraphicsOverlay.getGraphics().add(plane3D);
+
+      // instantiate a new camera controller which orbits a given geo element at a certain distance
+      orbitCameraController = new OrbitGeoElementCameraController(plane3D, 100.0);
+      // set up default camera position
+      // this controls the pitch of the camera
+      orbitCameraController.setCameraPitchOffset(85);
+      // this controls the heading of the camera
+      orbitCameraController.setCameraHeadingOffset(120);
+      sceneView.setCameraController(orbitCameraController);
+
+      if (sceneView.getCameraController() == orbitCameraController) {
+        activateCamera.setDisable(true);
+        activateGlobalView.setDisable(false);
+      } else if (sceneView.getCameraController() == GlobeCameraController){
+        activateCamera.setDisable(false);
+        activateGlobalView.setDisable(true);
+      }
+
+      activateCamera.setOnAction(event -> {
+        // create an orbit camera controller to restrict the view to the graphic
+        sceneView.setCameraController(orbitCameraController);
+      });
+
+      activateGlobalView.setOnAction(event -> {
+        sceneView.setCameraController(new GlobeCameraController());
+        Camera camera = new Camera(64.416919, -14.483728, 100, 318, 105, 0);
+        sceneView.setViewpointCamera(sceneView.getCurrentViewpointCamera());
+        System.out.println(sceneView.getCameraController());
+
+      });
+
+      sceneView.setOnMouseClicked(event -> {
+
+        System.out.println(sceneView.getCameraController());
+
+      });
 
 
 
 
       // add scene view to the stack pane
-      stackPane.getChildren().addAll(sceneView);
+      stackPane.getChildren().addAll(sceneView, controlsVBox);
+      StackPane.setAlignment(controlsVBox, Pos.TOP_RIGHT);
+      StackPane.setMargin(controlsVBox, new Insets(10, 0, 0, 10));
 
 
 
@@ -80,6 +173,13 @@ public class ControlTheCameraSample extends Application {
 
 
   }
+
+//  private void activateCamera() {
+//
+//    toggleCamera.setOnAction(event -> sceneView.setCameraController(orbitCameraController));
+//
+//
+//  }
 
   /**
    * Stops and releases all resources used in application.
