fa046f6b0dc9e60dbd2cbfc7de64977fb35cdaf8
Rachael Ellen <rellen@esri.com>
2019-01-10 15:56:45 +0000

    Update ControlTheCameraSample.java
    Add sliders to control camera operations

--- src/main/java/com/esri/samples/scene/control_the_camera/ControlTheCameraSample.java
+++ src/main/java/com/esri/samples/scene/control_the_camera/ControlTheCameraSample.java
@@ -26,40 +26,41 @@ import com.esri.arcgisruntime.mapping.Surface;
 
 import com.esri.arcgisruntime.mapping.Viewpoint;
 import com.esri.arcgisruntime.mapping.view.Camera;
-import com.esri.arcgisruntime.mapping.view.CameraController;
 import com.esri.arcgisruntime.mapping.view.GlobeCameraController;
 import com.esri.arcgisruntime.mapping.view.Graphic;
 import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.LayerSceneProperties;
 import com.esri.arcgisruntime.mapping.view.OrbitGeoElementCameraController;
-import com.esri.arcgisruntime.mapping.view.OrbitLocationCameraController;
 import com.esri.arcgisruntime.mapping.view.SceneView;
 import com.esri.arcgisruntime.symbology.ModelSceneSymbol;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
-import javafx.scene.shape.Circle;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
-import java.beans.EventHandler;
 import java.io.File;
 
 public class ControlTheCameraSample extends Application {
 
   private OrbitGeoElementCameraController orbitCameraController;
+  private GlobeCameraController globeCameraController;
   private SceneView sceneView;
   private Graphic plane3D;
   private static final SpatialReference WGS84 = SpatialReferences.getWgs84();
+  private Camera camera;
 
 
   @Override
@@ -97,15 +98,19 @@ public class ControlTheCameraSample extends Application {
       controlsVBox.setBackground(new Background(new BackgroundFill(Paint.valueOf("rgba(0, 0, 0, 0.3)"),
               CornerRadii.EMPTY, Insets.EMPTY)));
       controlsVBox.setPadding(new Insets(10.0));
-      controlsVBox.setMaxSize(260, 110);
+      controlsVBox.setMaxSize(180, 400);
       controlsVBox.getStyleClass().add("panel-region");
 
-      Button activateCamera = new Button("Fix camera to object");
-      Button activateGlobalView = new Button ("Free pan");
+      Button activateCameraButton = new Button("Fix camera to object");
+      activateCameraButton.setMaxWidth(Double.MAX_VALUE);
+      activateCameraButton.setDisable(true);
+      Button activateGlobalViewButton = new Button ("Exit Fixed Camera Mode");
+      activateGlobalViewButton.setMaxWidth(Double.MAX_VALUE);
 
-      Circle circle = new Circle(25);
-      controlsVBox.getChildren().addAll(circle, activateCamera, activateGlobalView);
-
+      Label label = new Label("Active Camera: Fixed view");
+      label.setWrapText(true);
+      label.setTextAlignment(TextAlignment.CENTER);
+      label.setMaxWidth(Double.MAX_VALUE);
 
       // create a graphics overlay for the scene
       GraphicsOverlay sceneGraphicsOverlay = new GraphicsOverlay();
@@ -120,6 +125,8 @@ public class ControlTheCameraSample extends Application {
       plane3D = new Graphic(new Point(6.637, 45.399, 1955, WGS84), plane3DSymbol);
       sceneGraphicsOverlay.getGraphics().add(plane3D);
 
+      globeCameraController = new GlobeCameraController();
+
       // instantiate a new camera controller which orbits a given geo element at a certain distance
       orbitCameraController = new OrbitGeoElementCameraController(plane3D, 100.0);
       // set up default camera position
@@ -127,43 +134,71 @@ public class ControlTheCameraSample extends Application {
       orbitCameraController.setCameraPitchOffset(85);
       // this controls the heading of the camera
       orbitCameraController.setCameraHeadingOffset(120);
+      // control max distance of orbit camera
+      orbitCameraController.setMaxCameraDistance(500);
+
       sceneView.setCameraController(orbitCameraController);
 
-      if (sceneView.getCameraController() == orbitCameraController) {
-        activateCamera.setDisable(true);
-        activateGlobalView.setDisable(false);
-      } else if (sceneView.getCameraController() == GlobeCameraController){
-        activateCamera.setDisable(false);
-        activateGlobalView.setDisable(true);
-      }
+      // slider for controlling heading direction
+      Slider headingSlider = new Slider(0, 360, 1);
+      headingSlider.setValue(120);
+      headingSlider.setShowTickMarks(true);
+      headingSlider.setMajorTickUnit(20);
+      headingSlider.setShowTickLabels(true);
 
-      activateCamera.setOnAction(event -> {
+      headingSlider.valueProperty().addListener( o -> {
+        orbitCameraController.setCameraHeadingOffset(headingSlider.getValue());
+      });
+
+      Slider pitchSlider = new Slider(0, 180, 1);
+      pitchSlider.setValue(85);
+      pitchSlider.setOrientation(Orientation.VERTICAL);
+      pitchSlider.setPrefHeight(5000);
+
+      pitchSlider.valueProperty().addListener( o -> {
+        orbitCameraController.setCameraPitchOffset(pitchSlider.getValue());
+      });
+
+      Slider distanceSlider = new Slider(10, 500, 1);
+      distanceSlider.setValue(orbitCameraController.getCameraDistance());
+
+      distanceSlider.valueProperty().addListener(o -> {
+        orbitCameraController.setCameraDistance(distanceSlider.getValue());
+      });
+
+
+      activateCameraButton.setOnAction(event -> {
         // create an orbit camera controller to restrict the view to the graphic
         sceneView.setCameraController(orbitCameraController);
+
+        activateCameraButton.setDisable(true);
+        activateGlobalViewButton.setDisable(false);
+        label.setText("Active Camera: Fixed view");
+        headingSlider.setVisible(true);
+        pitchSlider.setVisible(true);
       });
 
-      activateGlobalView.setOnAction(event -> {
-        sceneView.setCameraController(new GlobeCameraController());
-        Camera camera = new Camera(64.416919, -14.483728, 100, 318, 105, 0);
+
+      activateGlobalViewButton.setOnAction(event -> {
+        // create a globe camera controller to allow panning of the view across the scene
+        sceneView.setCameraController(globeCameraController);
+        // set the viewpoint to the current camera
         sceneView.setViewpointCamera(sceneView.getCurrentViewpointCamera());
-        System.out.println(sceneView.getCameraController());
 
+        activateGlobalViewButton.setDisable(true);
+        activateCameraButton.setDisable(false);
+        label.setText("Active Camera: Free view");
+        headingSlider.setVisible(false);
+        pitchSlider.setVisible(false);
       });
 
-      sceneView.setOnMouseClicked(event -> {
 
-        System.out.println(sceneView.getCameraController());
-
-      });
-
-
-
+      controlsVBox.getChildren().addAll(activateCameraButton, label, headingSlider, pitchSlider, distanceSlider, activateGlobalViewButton);
 
       // add scene view to the stack pane
       stackPane.getChildren().addAll(sceneView, controlsVBox);
       StackPane.setAlignment(controlsVBox, Pos.TOP_RIGHT);
-      StackPane.setMargin(controlsVBox, new Insets(10, 0, 0, 10));
-
+      StackPane.setMargin(controlsVBox, new Insets(10, 10, 0, 0));
 
 
     } catch (Exception e) {
@@ -174,12 +209,15 @@ public class ControlTheCameraSample extends Application {
 
   }
 
-//  private void activateCamera() {
-//
-//    toggleCamera.setOnAction(event -> sceneView.setCameraController(orbitCameraController));
-//
-//
-//  }
+  private Camera slowPan(Viewpoint viewpoint) {
+
+    Camera slowCamera = sceneView.getCurrentViewpointCamera();
+
+
+
+    return slowCamera;
+
+  }
 
   /**
    * Stops and releases all resources used in application.
