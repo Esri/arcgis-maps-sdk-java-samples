a0575aadfc242c099e9a98849a9e805ac2d4ed45
Tyler Schiewe <tyler.schiewe@gmail.com>
2019-01-31 11:22:33 -0800

    add enc exchange set sample

---
+++ src/main/java/com/esri/samples/hydrography/add_enc_exchange_set/AddEncExchangeSetSample.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2019 Esri.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.esri.samples.hydrography.add_enc_exchange_set;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import com.esri.arcgisruntime.geometry.Envelope;
+import com.esri.arcgisruntime.geometry.GeometryEngine;
+import com.esri.arcgisruntime.hydrography.EncCell;
+import com.esri.arcgisruntime.hydrography.EncDataset;
+import com.esri.arcgisruntime.hydrography.EncExchangeSet;
+import com.esri.arcgisruntime.layers.EncLayer;
+import com.esri.arcgisruntime.loadable.LoadStatus;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.Viewpoint;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+public class AddEncExchangeSetSample extends Application {
+
+  private MapView mapView;
+  private Envelope completeExtent;
+
+  @Override
+  public void start(Stage stage) {
+
+    try {
+      // set the title and width of the stage
+      stage.setTitle("Add ENC Exchange Set Sample");
+      stage.setWidth(800);
+      stage.setHeight(700);
+
+      // create a stack pane and set it as the JavaFX scene's root
+      StackPane stackPane = new StackPane();
+      Scene scene = new Scene(stackPane);
+      stage.setScene(scene);
+      stage.show();
+
+      // create a map view and add it to the stack pane
+      mapView = new MapView();
+      stackPane.getChildren().add(mapView);
+
+      // create a map with the Oceans basemap and set it on the map view
+      ArcGISMap map = new ArcGISMap(Basemap.createOceans());
+      mapView.setMap(map);
+
+      // load the ENC exchange set from local data
+      File encPath = new File("./samples-data/enc/ExchangeSetwithoutUpdates/ENC_ROOT/CATALOG.031");
+      EncExchangeSet encExchangeSet = new EncExchangeSet(Collections.singletonList(encPath.getAbsolutePath()));
+      encExchangeSet.loadAsync();
+      encExchangeSet.addDoneLoadingListener(() -> {
+        if (encExchangeSet.getLoadStatus() == LoadStatus.LOADED) {
+          // loop through the individual datasets of the exchange set
+          for (EncDataset encDataset : encExchangeSet.getDatasets()) {
+            // create an ENC layer with an ENC cell using the dataset
+            EncLayer encLayer = new EncLayer(new EncCell(encDataset));
+            // add the ENC layer to the map's operational layers to display it
+            map.getOperationalLayers().add(encLayer);
+            // combine the extents of each layer after loading to set the viewpoint to their complete extent
+            encLayer.addDoneLoadingListener(() -> {
+              if (encLayer.getLoadStatus() == LoadStatus.LOADED) {
+                Envelope extent = encLayer.getFullExtent();
+                if (completeExtent == null) {
+                  completeExtent = extent;
+                } else {
+                  completeExtent = GeometryEngine.combineExtents(Arrays.asList(completeExtent, extent));
+                }
+                mapView.setViewpoint(new Viewpoint(completeExtent));
+              } else {
+                new Alert(Alert.AlertType.ERROR, "Failed to load ENC layer.").show();
+              }
+            });
+          }
+        } else {
+          new Alert(Alert.AlertType.ERROR, "Failed to load ENC exchange set.").show();
+        }
+      });
+    } catch (Exception e) {
+      // on any error, display the stack trace.
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Stops and releases all resources used in application.
+   */
+  @Override
+  public void stop() {
+
+    if (mapView != null) {
+      mapView.dispose();
+    }
+  }
+
+  /**
+   * Opens and runs application.
+   *
+   * @param args arguments passed to this application
+   */
+  public static void main(String[] args) {
+
+    Application.launch(args);
+  }
+
+}
+
---
+++ src/main/java/com/esri/samples/hydrography/add_enc_exchange_set/README.md
@@ -0,0 +1,46 @@
+<h1>Add ENC exchange set</h1>
+
+<p>Display nautical charts conforming to the ENC specification.</p>
+
+<p><img src="AddEncExchangeSet.png"/></p>
+
+<h2>Use case</h2>
+
+<p>Maritime applications require conformity to strict specifications over how hydrographic data is displayed digitally to
+ensure the safety of traveling vessels.</p>
+
+<p>S57 is the IHO (International Hydrographic Organization) Transfer Standard for Digital Hydrographic Data. The symbology
+standard for this is called S52. There are different product specifications for this standard. ENC is one such
+specification developed by IHO.</p>
+
+<p>An ENC exchange set is a catalog of data files which can be loaded as cells. The cells contain information on how
+symbols should be displayed in relation to one another, so as to represent information such as depth and obstacles
+accurately.</p>
+
+<h2>How it works</h2>
+
+<ol>
+<li>Specify the path to a local CATALOG file to create an <code>EncExchangeSet</code>.</li>
+
+<li>After loading the exchange set, loop through the <code>EncDataset</code> objects in <code>encExchangeSet.getDatasets()</code>.</li>
+
+<li>Create an <code>EncCell</code> for each dataset. Then create an <code>EncLayer</code> for each cell.</li>
+
+<li>Add the ENC layer to a map's operational layers collection to display it.</li>
+</ol>
+
+<h2>Relevant API</h2>
+
+<ul>
+<li>EncCell</li>
+
+<li>EncDataset</li>
+
+<li>EncExchangeSet</li>
+
+<li>EncLayer</li>
+</ul>
+
+<h2>Tags</h2>
+
+<p>Data, ENC, maritime, nautical chart, layers, hydrographic</p>
