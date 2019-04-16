2235690c89ce44d6efbb2f84e094f8e03fbed3d8
Rachael Ellen <rellen@esri.com>
2019-04-16 17:53:23 +0100

    Add copyright statement, remove redundant code commenting

--- src/main/java/com/esri/samples/ogc/display_wfs_layer/DisplayWFSLayerSample.java
+++ src/main/java/com/esri/samples/ogc/display_wfs_layer/DisplayWFSLayerSample.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Esri.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.esri.samples.ogc.display_wfs_layer;
 
 import com.esri.arcgisruntime.data.QueryParameters;
@@ -77,7 +93,7 @@ public class DisplayWFSLayerSample extends Application {
     });
 
     // add the mapview to the stackpane
-    stackPane.getChildren().addAll(mapView);
+    stackPane.getChildren().add(mapView);
   }
 
   /**
@@ -100,7 +116,6 @@ public class DisplayWFSLayerSample extends Application {
    */
   @Override
   public void stop() {
-    // release resources when the application closes
     if (mapView != null) {
       mapView.dispose();
     }
