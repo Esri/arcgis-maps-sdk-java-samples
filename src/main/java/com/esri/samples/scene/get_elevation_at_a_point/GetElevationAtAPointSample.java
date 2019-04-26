430546f10bb0b0da7db787e9e337ddd7cfcedb57
Jonathan Lavi <jlavi@esri.com>
2019-04-26 17:33:47 +0100

    add vbox for label, set font size, add screenshot

--- src/main/java/com/esri/samples/scene/get_elevation_at_a_point/GetElevationAtAPointSample.java
+++ src/main/java/com/esri/samples/scene/get_elevation_at_a_point/GetElevationAtAPointSample.java
@@ -16,38 +16,44 @@
 
 package com.esri.samples.scene.get_elevation_at_a_point;
 
-import com.esri.arcgisruntime.geometry.Point;
-import com.esri.arcgisruntime.mapping.view.Graphic;
-import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
+import java.util.concurrent.ExecutionException;
+
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Point2D;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseButton;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import com.esri.arcgisruntime.concurrent.ListenableFuture;
+import com.esri.arcgisruntime.geometry.Point;
 import com.esri.arcgisruntime.mapping.ArcGISScene;
 import com.esri.arcgisruntime.mapping.ArcGISTiledElevationSource;
 import com.esri.arcgisruntime.mapping.Basemap;
-import com.esri.arcgisruntime.mapping.Surface;
-import com.esri.arcgisruntime.mapping.view.Camera;
 import com.esri.arcgisruntime.mapping.view.SceneView;
+import com.esri.arcgisruntime.mapping.view.Camera;
+import com.esri.arcgisruntime.mapping.view.Graphic;
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
+import com.esri.arcgisruntime.mapping.Surface;
 import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
 
 public class GetElevationAtAPointSample extends Application {
 
     private SceneView sceneView;
-    private static final String ELEVATION_IMAGE_SERVICE =
-            "http://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer";
 
     @Override
-    public void start(Stage stage){
+    public void start(Stage stage) {
 
         try {
-
-            // create stack pane and JavaFX app scene
+            // create stack pane and application scene
             StackPane stackPane = new StackPane();
             Scene fxScene = new Scene(stackPane);
+            fxScene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
 
             // set title, size, and add JavaFX scene to stage
             stage.setTitle("Get Elevation At A Point Sample");
@@ -60,10 +66,28 @@ public class GetElevationAtAPointSample extends Application {
             ArcGISScene scene = new ArcGISScene();
             scene.setBasemap(Basemap.createImagery());
 
-            // add the SceneView to the stack pane
+            // create a control panel
+            VBox controlsVBox = new VBox(6);
+            controlsVBox.setBackground(new Background(new BackgroundFill(Paint.valueOf("rgba(0,0,0,0.6)"), CornerRadii.EMPTY,
+                    Insets.EMPTY)));
+            controlsVBox.setPadding(new Insets(10.0));
+            controlsVBox.setMaxSize(220, 90);
+            controlsVBox.getStyleClass().add("panel-region");
+
+            // add the SceneView and controlsVBox to the stack pane
             sceneView = new SceneView();
             sceneView.setArcGISScene(scene);
-            stackPane.getChildren().addAll(sceneView);
+            stackPane.getChildren().addAll(sceneView, controlsVBox);
+            StackPane.setAlignment(controlsVBox, Pos.BOTTOM_CENTER);
+            StackPane.setMargin(controlsVBox, new Insets(10, 0, 0, 10));
+
+            // create label to display the elevation
+            Label elevationLabel = new Label("Elevation:");
+            elevationLabel.setFont(new Font(22));
+            elevationLabel.getStyleClass().add("panel-label");
+
+            // add label to the control panel
+            controlsVBox.getChildren().add(elevationLabel);
 
             // add base surface for elevation data
             Surface surface = new Surface();
@@ -91,26 +115,32 @@ public class GetElevationAtAPointSample extends Application {
                 Point surfacePoint = sceneView.screenToBaseSurface(screenpoint);
 
                 // check that the point is on the surface, and primary button was clicked
-                if (surfacePoint != null && event.getButton() == MouseButton.PRIMARY){
+                if (surfacePoint != null && event.getButton() == MouseButton.PRIMARY) {
 
                     // clear any existing graphics from the graphics overlay
                     graphicsOverlay.getGraphics().clear();
 
                     // create a new graphic at the surface point and add it to the graphics overlay
-
                     Graphic surfacePointGraphic = new Graphic(surfacePoint, circleSymbol);
                     graphicsOverlay.getGraphics().add(surfacePointGraphic);
 
                     // get the surface elevation at the surface point
+                    ListenableFuture<Double> elevationFuture = scene.getBaseSurface().getElevationAsync(surfacePoint);
+                    elevationFuture.addDoneListener(() -> {
+                                try {
+                                    Double elevation = elevationFuture.get();
 
+                                    // update the label text with the new elevation
+                                    elevationLabel.setText("Elevation: " + Math.round(elevation) + " m");
 
-                    // display a marker
+                                } catch (InterruptedException | ExecutionException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                    );
                 }
-
             });
-        }
-
-        catch (Exception e){
+        } catch (Exception e) {
             // on any error, display the stack trace.
             e.printStackTrace();
         }
