05401abacb4ac21c0fc3af696886d6ff579ee576
Jonathan Lavi <jlavi@esri.com>
2019-04-29 17:11:31 +0100

    remove HashMap, find rule through list index@

--- src/main/java/com/esri/samples/raster/raster_rendering_rule/RasterRenderingRuleSample.java
+++ src/main/java/com/esri/samples/raster/raster_rendering_rule/RasterRenderingRuleSample.java
@@ -1,4 +1,22 @@
+/* Copyright 2019 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.esri.samples.raster.raster_rendering_rule;
+
+import java.util.List;
 
 import com.esri.arcgisruntime.arcgisservices.RenderingRuleInfo;
 import com.esri.arcgisruntime.layers.RasterLayer;
@@ -17,16 +35,12 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import java.util.HashMap;
-import java.util.List;
-
 public class RasterRenderingRuleSample extends Application {
 
     private ComboBox<String> renderingRuleDropDownMenu;
     private MapView mapView;
     private ArcGISMap map;
     final String imageServiceUrl = "http://sampleserver6.arcgisonline.com/arcgis/rest/services/CharlotteLAS/ImageServer";
-    private HashMap<String, RenderingRuleInfo> renderingRuleInfoHashMap;
 
     @Override
     public void start(Stage stage) {
@@ -74,15 +88,12 @@ public class RasterRenderingRuleSample extends Application {
                     //populate the drop down menu with the rendering rule names
                     populateDropDownMenu(renderingRuleInfos);
 
-                    // build a rendering rule hashmap and add the names of the rules to the drop down menu
-                    renderingRuleInfoHashMap = buildRenderingRuleInfoHashMap(renderingRuleInfos);
-
                     // listen to selection in the drop-down menu
                     renderingRuleDropDownMenu.getSelectionModel().selectedItemProperty().addListener(o -> {
-                        // save the selected rendering rule name to a string
-                        String selectedRenderingRuleName = renderingRuleDropDownMenu.getSelectionModel().getSelectedItem();
+                        // get the selected rendering rule
+                        int selectedRenderingRuleIndex = renderingRuleDropDownMenu.getSelectionModel().getSelectedIndex();
                         // apply the selected rendering rule
-                        applyRenderingRule(selectedRenderingRuleName);
+                        applyRenderingRule(renderingRuleInfos, selectedRenderingRuleIndex);
                     });
                 }
             });
@@ -98,8 +109,8 @@ public class RasterRenderingRuleSample extends Application {
     }
 
     /*
-     *   populate the drop down menu with the rendering rule names
-     *   @param renderingRuleInfos list of rendering rule infos to add to the drop-down
+     * Populate the drop down menu with the rendering rule names
+     * @param renderingRuleInfos list of rendering rule infos to add to the drop-down
      */
 
     private void populateDropDownMenu(List<RenderingRuleInfo> renderingRuleInfos){
@@ -112,29 +123,14 @@ public class RasterRenderingRuleSample extends Application {
     }
 
     /*
-     * Build a HashMap of rendering rule infos
-     * @param renderingRuleInfos list of rendering rule infos
-     */
-
-    private HashMap<String, RenderingRuleInfo> buildRenderingRuleInfoHashMap(List<RenderingRuleInfo> renderingRuleInfos){
-        HashMap<String, RenderingRuleInfo> result = new HashMap<>();
-        for (RenderingRuleInfo renderingRuleInfo : renderingRuleInfos){
-            // get the name of the rendering rule
-            String renderingRuleName = renderingRuleInfo.getName();
-            // add the rendering rule to the hash map with the name as key
-            result.put(renderingRuleName, renderingRuleInfo);
-        }
-        return result;
-    }
-
-    /*
      * Apply a rendering rule on a Raster and add it to the map
-     * @param renderingRuleName name
+     * @param renderingRuleInfos    list of rendering rule infos to select from
+     * @param index                 selected drop-down menu item representing the rule to apply
      */
 
-    private void applyRenderingRule(String renderingRuleName){
-        // find the appropriate rendering rule info in the hash map of rendering rule infos
-        RenderingRuleInfo selectedRenderingRuleInfo = renderingRuleInfoHashMap.get(renderingRuleName);
+    private void applyRenderingRule(List<RenderingRuleInfo> renderingRuleInfos, int index){
+        // get the requested rendering rule info from the list
+        RenderingRuleInfo selectedRenderingRuleInfo = renderingRuleInfos.get(index);
         // clear all rasters
         map.getOperationalLayers().clear();
         // create a rendering rule object using the rendering rule info
