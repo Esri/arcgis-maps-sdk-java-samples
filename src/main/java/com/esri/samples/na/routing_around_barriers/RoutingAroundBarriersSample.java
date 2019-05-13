c14113eb3a7d153f2349aaace7b80b64686c62fc
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-05-13 10:00:27 +0100

    render street map and add controls panel

--- src/main/java/com/esri/samples/na/routing_around_barriers/RoutingAroundBarriersSample.java
+++ src/main/java/com/esri/samples/na/routing_around_barriers/RoutingAroundBarriersSample.java
@@ -1,11 +1,19 @@
 package com.esri.samples.na.routing_around_barriers;
 
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 public class RoutingAroundBarriersSample extends Application {
+
+  MapView mapView;
 
   @Override
   public void start(Stage stage) {
@@ -22,6 +30,28 @@ public class RoutingAroundBarriersSample extends Application {
       stage.setHeight(700);
       stage.setScene(scene);
       stage.show();
+
+      // create a control panel
+      VBox controlsVBox = new VBox(6);
+      controlsVBox.setBackground(new Background(new BackgroundFill(Paint.valueOf("rgba(0,0,0,0.3)"), CornerRadii.EMPTY, Insets.EMPTY)));
+      controlsVBox.setPadding(new Insets(10.0));
+      controlsVBox.setMaxSize(400,300);
+      controlsVBox.getStyleClass().add("panel-region");
+
+
+
+
+      // create an ArcGISMap with a streets basemap
+      ArcGISMap map = new ArcGISMap(Basemap.createStreets());
+
+      // set the ArcGISMap to be displayed in this view
+      mapView = new MapView();
+      mapView.setMap(map);
+
+      // add the map view and control panel to the stack pane
+      stackPane.getChildren().addAll(mapView, controlsVBox);
+      StackPane.setAlignment(controlsVBox, Pos.TOP_LEFT);
+      StackPane.setMargin(controlsVBox, new Insets(10,0,0,10));
 
     } catch (Exception e) {
       e.printStackTrace();
