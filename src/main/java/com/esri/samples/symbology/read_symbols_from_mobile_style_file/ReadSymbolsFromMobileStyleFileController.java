35e2c0314f4ca4d49d4bd582f4644fd73ccd4d8a
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-05-29 14:44:21 +0100

    load and search stylx file

--- src/main/java/com/esri/samples/symbology/read_symbols_from_mobile_style_file/ReadSymbolsFromMobileStyleFileController.java
+++ src/main/java/com/esri/samples/symbology/read_symbols_from_mobile_style_file/ReadSymbolsFromMobileStyleFileController.java
@@ -16,11 +16,21 @@
 
 package com.esri.samples.symbology.read_symbols_from_mobile_style_file;
 
-import javafx.fxml.FXML;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+
+import com.esri.arcgisruntime.concurrent.ListenableFuture;
+import com.esri.arcgisruntime.loadable.LoadStatus;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.symbology.SymbolStyle;
+import com.esri.arcgisruntime.symbology.SymbolStyleSearchParameters;
+import com.esri.arcgisruntime.symbology.SymbolStyleSearchResult;
 
 public class ReadSymbolsFromMobileStyleFileController {
 
@@ -34,7 +44,67 @@ public class ReadSymbolsFromMobileStyleFileController {
     ArcGISMap map = new ArcGISMap(Basemap.createTopographic());
     // add the map to the map view
     mapView.setMap(map);
-    
+
+    // create a graphics overlay and add it to the map
+    GraphicsOverlay graphicsOverlay = new GraphicsOverlay();
+    mapView.getGraphicsOverlays().add(graphicsOverlay);
+
+    loadSymbolsFromStyleFile();
+
+  }
+
+  /**
+   * Loads the stylx file and searches for all symbols contained within. Put the resulting symbols into the GUI
+   * based on their category (eyes, mouth, hat, face).
+   */
+  private void loadSymbolsFromStyleFile(){
+    // create a SymbolStyle by passing the location of the .stylx file in the constructor
+    SymbolStyle emojiStyle = new SymbolStyle("./samples-data/stylx/emoji-mobile.stylx");
+    emojiStyle.loadAsync();
+
+    // add a listener to run when the SymbolStyle has loaded
+    emojiStyle.addDoneLoadingListener(() -> {
+      if (emojiStyle.getLoadStatus() == LoadStatus.FAILED_TO_LOAD){
+        new Alert(Alert.AlertType.ERROR, "Error: could not load .stylx file. Details: "+ emojiStyle.getLoadError().getMessage()).show();
+        return;
+      }
+      
+      // load the default search parameters
+      ListenableFuture<SymbolStyleSearchParameters> defaultSearchParametersFuture = emojiStyle.getDefaultSearchParametersAsync();
+      defaultSearchParametersFuture.addDoneListener(()->{
+        try {
+          SymbolStyleSearchParameters defaultSearchParameters = defaultSearchParametersFuture.get();
+
+          // use the default parameters to perform the search
+          ListenableFuture<List<SymbolStyleSearchResult>> symbolStyleSearchResultFuture = emojiStyle.searchSymbolsAsync(defaultSearchParameters);
+          symbolStyleSearchResultFuture.addDoneListener(()->{
+            try {
+              List<SymbolStyleSearchResult> symbolStyleSearchResults = symbolStyleSearchResultFuture.get();
+              for (SymbolStyleSearchResult symbolStyleSearchResult : symbolStyleSearchResults) {
+                switch (symbolStyleSearchResult.getCategory().toLowerCase()) {
+                  case "eyes":
+                    System.out.println("eyes");
+                    break;
+                  case "mouth":
+                    System.out.println("mouth");
+                    break;
+                  case "hat":
+                    System.out.println("hat");
+                    break;
+                  case "face":
+                    System.out.println("face");
+                    break;
+                }
+              }
+            } catch (InterruptedException | ExecutionException e) {
+              new Alert(Alert.AlertType.ERROR, "Error performing the symbol search"+ e.getMessage()).show();
+            }
+          });
+        } catch (InterruptedException | ExecutionException e) {
+          new Alert(Alert.AlertType.ERROR, "Error retrieving default search parameters for symbol search"+ e.getMessage()).show();
+        }
+      });
+    });
   }
 
   /**
