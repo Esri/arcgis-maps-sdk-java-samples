a75f46bc0fac6d0ba92b5666d1f9a10039d8f4f1
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-06-12 12:26:40 +0100

    indentation

--- src/main/java/com/esri/samples/na/routing_around_barriers/RoutingAroundBarriersSample.java
+++ src/main/java/com/esri/samples/na/routing_around_barriers/RoutingAroundBarriersSample.java
@@ -29,35 +29,35 @@ public class RoutingAroundBarriersSample extends Application {
   @Override
   public void start(Stage stage) throws Exception {
 
-      // set up the scene
-      FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/routing_around_barriers.fxml"));
-      Parent root = loader.load();
-      controller = loader.getController();
-      Scene scene = new Scene(root);
+    // set up the scene
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/routing_around_barriers.fxml"));
+    Parent root = loader.load();
+    controller = loader.getController();
+    Scene scene = new Scene(root);
 
-      // set title, size and add scene to stage
-      stage.setTitle("Routing Around Barriers Sample");
-      stage.setWidth(800);
-      stage.setHeight(700);
-      stage.setScene(scene);
-      stage.show();
-    }
-
-    /**
-     * Stops and releases all resources used in application.
-     */
-    @Override
-    public void stop () { controller.terminate(); }
-
-    /**
-     * Opens and runs application.
-     *
-     * @param args arguments passed to this application
-     */
-    public static void main (String[]args){
-
-      Application.launch(args);
-    }
+    // set title, size and add scene to stage
+    stage.setTitle("Routing Around Barriers Sample");
+    stage.setWidth(800);
+    stage.setHeight(700);
+    stage.setScene(scene);
+    stage.show();
   }
+
+  /**
+   * Stops and releases all resources used in application.
+   */
+  @Override
+  public void stop () { controller.terminate(); }
+
+  /**
+   * Opens and runs application.
+   *
+   * @param args arguments passed to this application
+   */
+  public static void main (String[]args){
+
+    Application.launch(args);
+  }
+}
 
 
