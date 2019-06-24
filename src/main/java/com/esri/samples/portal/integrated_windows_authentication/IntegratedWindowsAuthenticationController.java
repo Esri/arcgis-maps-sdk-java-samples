524f97e5828fdac3f3b5448c8289a11c8b89ce0a
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-06-24 15:15:29 +0100

    comments, formatting, UI improvements

--- src/main/java/com/esri/samples/portal/integrated_windows_authentication/IntegratedWindowsAuthenticationController.java
+++ src/main/java/com/esri/samples/portal/integrated_windows_authentication/IntegratedWindowsAuthenticationController.java
@@ -1,13 +1,37 @@
+/*
+ * Copyright 2019 Esri.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.esri.samples.portal.integrated_windows_authentication;
 
+import java.io.ByteArrayInputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
 import com.esri.arcgisruntime.concurrent.ListenableFuture;
 import com.esri.arcgisruntime.loadable.LoadStatus;
@@ -22,14 +46,12 @@ import com.esri.arcgisruntime.security.AuthenticationManager;
 
 public class IntegratedWindowsAuthenticationController {
 
-  @FXML
-  private MapView mapView;
-  @FXML
-  private ListView<PortalItem> resultsListView;
-  @FXML
-  private TextField portalUrlTextField;
-  @FXML
-  private Label loadStateTextView;
+  @FXML private MapView mapView;
+  @FXML private ListView<PortalItem> resultsListView;
+  @FXML private TextField portalUrlTextField;
+  @FXML private ProgressIndicator progressIndicator;
+  @FXML private Text loadStateTextView;
+  @FXML private Text loadWebMapTextView;
 
   public void initialize()  {
     try {
@@ -46,12 +68,41 @@ public class IntegratedWindowsAuthenticationController {
       // add a listener to the map results list view that loads the map on selection
       resultsListView.getSelectionModel().selectedItemProperty().addListener((obs, oldValue, newValue) -> displayMap());
 
+      // make the list view show a preview of the portal items' map area
+      resultsListView.setCellFactory(c -> new PortalItemListCell());
+
     } catch (Exception e) {
       // on any error, display the stack trace.
       e.printStackTrace();
     }
   }
 
+  /**
+   * Handles searching a public portal
+   */
+  @FXML
+  private void handleSearchPublicPress(){
+    searchPortal(new Portal("http://www.arcgis.com"));
+  }
+
+  /**
+   * Handles searching a secure portal
+   */
+  @FXML
+  private void handleSearchSecurePress(){
+    // check for a url in the URL field
+    if (!portalUrlTextField.getText().isEmpty()) {
+      // search an instance of the IWA-secured portal, the user may be challenged for access
+      searchPortal(new Portal(portalUrlTextField.getText(), true));
+    } else {
+      new Alert(Alert.AlertType.ERROR, "Portal URL is empty. Please enter a portal URL.").show();
+    }
+  }
+
+  /**
+   * Search the given portal for its portal items and display them in a list view.
+   * @param portal
+   */
   private void searchPortal(Portal portal){
 
     // check if the portal is null
@@ -63,16 +114,27 @@ public class IntegratedWindowsAuthenticationController {
     // clear any existing items in the list view
     resultsListView.getItems().clear();
 
+    // show portal load state
+    progressIndicator.setVisible(true);
+    loadStateTextView.setText("Searching for web map items on the portal at " + portal.getUri());
+
     // load the portal items
     portal.loadAsync();
     portal.addDoneLoadingListener(()->{
       if (portal.getLoadStatus() == LoadStatus.LOADED) {
-        // update load state in UI with the portal URI
+        try {
+          // update load state in UI with the portal URI
+          loadStateTextView.setText("Connected to the portal on " + new URI(portal.getUri()).getHost());
+        } catch (URISyntaxException e) {
+          new Alert(Alert.AlertType.ERROR, "Error getting URI from portal: " + e.getMessage()).show();
+        }
 
         // report the user name used for this connection
         if (portal.getUser() != null){
-          System.out.println(portal.getUser().getUsername());
           loadStateTextView.setText("Connected as: " + portal.getUser().getUsername());
+        } else {
+          // for a secure portal, the user should never be anonymous
+          loadStateTextView.setText("Connected as: Anonymous");
         }
 
         // search the portal for web maps
@@ -84,39 +146,31 @@ public class IntegratedWindowsAuthenticationController {
             List<PortalItem> portalItems = portalItemSet.getResults();
             // add the items to the list view
             portalItems.forEach(portalItem -> resultsListView.getItems().add(portalItem));
-
           } catch (ExecutionException | InterruptedException e) {
             new Alert(Alert.AlertType.ERROR,  "Error getting portal item set from portal: " + e.getMessage()).show();
           }
+          // hide the progress indicator
+          progressIndicator.setVisible(false);
         });
 
       } else {
+        // hide the progress indicator
+        progressIndicator.setVisible(false);
         // report error
         new Alert(Alert.AlertType.ERROR, "Portal sign in failed: " + portal.getLoadError().getCause().getMessage()).show();
       }
     });
   }
 
+  /**
+   * Gets the selected Portal item from the list view, creates a new map from it and displays it in the map biew
+   */
   private void displayMap(){
-    PortalItem selectedItem = resultsListView.getSelectionModel().getSelectedItem();
-    System.out.println(selectedItem);
-    ArcGISMap webMap = new ArcGISMap(selectedItem);
-    mapView.setMap(webMap);
-  }
-
-  @FXML
-  private void handleSearchPublicPress(){
-    searchPortal(new Portal("http://www.arcgis.com"));
-  }
-
-  @FXML
-  private void handleSearchSecurePress(){
-
-    if (!portalUrlTextField.getText().isEmpty()) {
-      // search an instance of the IWA-secured portal, the user may be challenged for access
-      searchPortal(new Portal(portalUrlTextField.getText(), true));
-    } else {
-      new Alert(Alert.AlertType.ERROR, "Portal URL is empty. Please enter a portal URL.").show();
+    if (resultsListView.getSelectionModel().getSelectedItem() != null) {
+      PortalItem selectedItem = resultsListView.getSelectionModel().getSelectedItem();
+      ArcGISMap webMap = new ArcGISMap(selectedItem);
+      mapView.setMap(webMap);
+      loadWebMapTextView.setText("Loaded web map from item " + selectedItem.getItemId());
     }
   }
 
@@ -127,6 +181,43 @@ public class IntegratedWindowsAuthenticationController {
 
     if (mapView != null) {
       mapView.dispose();
+    }
+  }
+
+  /**
+   * Shows the available thumbnail of the PortalItem map in the selection list view.
+   */
+  class PortalItemListCell extends ListCell<PortalItem> {
+
+    @Override
+    protected void updateItem(PortalItem portalItem, boolean empty) {
+      super.updateItem(portalItem, empty);
+      if (portalItem != null) {
+        HBox hBox = new HBox();
+        hBox.setMinHeight(65);
+
+        // show the preplanned map area thumbnail image
+        ImageView thumbnailImageView = new ImageView();
+        hBox.getChildren().add(thumbnailImageView);
+        thumbnailImageView.setFitHeight(65);
+        thumbnailImageView.setPreserveRatio(true);
+        ListenableFuture<byte[]> thumbnailData = portalItem.fetchThumbnailAsync();
+        thumbnailData.addDoneListener(() -> {
+          try {
+            thumbnailImageView.setImage(new Image(new ByteArrayInputStream(thumbnailData.get())));
+          } catch (Exception ex) {
+            ex.printStackTrace();
+          }
+        });
+
+        setGraphic(hBox);
+
+        // set the list cell's text to the map's index
+        setText(portalItem.getTitle());
+      } else {
+        setGraphic(null);
+        setText(null);
+      }
     }
   }
 }
