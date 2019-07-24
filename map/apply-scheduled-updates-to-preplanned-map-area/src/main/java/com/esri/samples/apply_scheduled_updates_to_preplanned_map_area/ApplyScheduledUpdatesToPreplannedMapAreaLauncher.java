0e563698543999345e51d3bb8dca83468438d00c
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-07-24 09:36:07 +0100

    fix launcher

--- map/apply-scheduled-updates-to-preplanned-map-area/src/main/java/com/esri/samples/apply_scheduled_updates_to_preplanned_map_area/ApplyScheduledUpdatesToPreplannedMapAreaLauncher.java
+++ map/apply-scheduled-updates-to-preplanned-map-area/src/main/java/com/esri/samples/apply_scheduled_updates_to_preplanned_map_area/ApplyScheduledUpdatesToPreplannedMapAreaLauncher.java
@@ -3,9 +3,9 @@ package com.esri.samples.apply_scheduled_updates_to_preplanned_map_area;
 /**
 * Wrapper required for launching a JavaFX 11 app through Gradle or from a jar.
 */
-public class AccessLoadStatusLauncher {
+public class ApplyScheduledUpdatesToPreplannedMapAreaLauncher {
 
   public static void main(String[] args) {
-    AccessLoadStatusSample.main(args); 
+    ApplyScheduledUpdatesToPreplannedMapAreaSample.main(args);
   }
 }
