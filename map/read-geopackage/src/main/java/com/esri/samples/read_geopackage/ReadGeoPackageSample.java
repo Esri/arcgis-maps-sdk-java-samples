e12e4915937ca4fde87d6712edff124e5e7f548f
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-07-24 10:11:04 +0100

    remove css stylesheet

--- map/read-geopackage/src/main/java/com/esri/samples/read_geopackage/ReadGeoPackageSample.java
+++ map/read-geopackage/src/main/java/com/esri/samples/read_geopackage/ReadGeoPackageSample.java
@@ -43,7 +43,6 @@ public class ReadGeoPackageSample extends Application {
       // create stack pane and application scene
       StackPane stackPane = new StackPane();
       Scene scene = new Scene(stackPane);
-      scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
 
       // set title, size, and add scene to stage
       stage.setTitle("Read GeoPackage Sample");
