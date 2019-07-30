6c71c77fad1659e8c5a13bbfb69696846503f5fa
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-07-30 08:52:29 +0100

    implement review changes

--- portal/oauth/src/main/java/com/esri/samples/oauth/OAuthSample.java
+++ portal/oauth/src/main/java/com/esri/samples/oauth/OAuthSample.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Esri.
+ * Copyright 2017 Esri.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -14,7 +14,7 @@
  * the License.
  */
 
-package com.esri.samples.portal.oauth;
+package com.esri.samples.oauth;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
