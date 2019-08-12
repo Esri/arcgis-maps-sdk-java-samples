3ff8ed3e809364534f54269930ed885bf770ed57
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-08-12 16:25:06 +0100

    split to mvc and add fxml file

---
+++ kml/create-and-save-kml-file/src/main/java/com/esri/samples/create_and_save_kml_file/CreateAndSaveKMLFileController.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2019 Esri.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.esri.samples.create_and_save_kml_file;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ColorPicker;
+
+import com.esri.arcgisruntime.layers.KmlLayer;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.ogc.kml.KmlDataset;
+import com.esri.arcgisruntime.ogc.kml.KmlDocument;
+import com.esri.arcgisruntime.ogc.kml.KmlPlacemark;
+
+public class CreateAndSaveKMLFileController {
+
+  @FXML
+  private MapView mapView;
+
+  private ArcGISMap map;
+  private KmlDocument kmlDocument;
+  private KmlDataset kmlDataset;
+  private KmlLayer kmlLayer;
+  private KmlPlacemark currentKmlPlacemark;
+
+  @FXML
+  public void initialize() {
+
+    // create a map and add it to the map view
+    map = new ArcGISMap(Basemap.createDarkGrayCanvasVector());
+    mapView.setMap(map);
+
+    ColorPicker colorPicker = new ColorPicker();
+
+    // set the images for the point icon picker
+    List<String> iconLinks = Arrays.asList(
+            "http://static.arcgis.com/images/Symbols/Shapes/BlueCircleLargeB.png",
+            "http://static.arcgis.com/images/Symbols/Shapes/BlueDiamondLargeB.png",
+            "http://static.arcgis.com/images/Symbols/Shapes/BluePin1LargeB.png",
+            "http://static.arcgis.com/images/Symbols/Shapes/BluePin2LargeB.png",
+            "http://static.arcgis.com/images/Symbols/Shapes/BlueSquareLargeB.png",
+            "http://static.arcgis.com/images/Symbols/Shapes/BlueStarLargeB.png");
+
+    // set up a new KML document and layer
+//    resetKmlDocument();
+  }
+
+  /**
+   *
+   */
+  private void resetKmlDocument() {
+    // clear any existing layers from the map
+    map.getOperationalLayers().clear();
+
+    // reset the most recently placed placemark
+    currentKmlPlacemark = null;
+
+    // create a new KmlDocument
+    kmlDocument = new KmlDocument();
+    kmlDocument.setName("KML Sample Document");
+
+    // create a kml dataset uting the kml document
+    kmlDataset = new KmlDataset(kmlDocument);
+
+    // create the kml layer using the kml dataset.
+    kmlLayer = new KmlLayer(kmlDataset);
+
+    // add the kml layer to the map
+    map.getOperationalLayers().add(kmlLayer);
+  }
+
+
+  /**
+   * Stops and releases all resources used in application.
+   */
+  public void terminate() {
+
+    if (mapView != null) {
+      mapView.dispose();
+    }
+  }
+}
