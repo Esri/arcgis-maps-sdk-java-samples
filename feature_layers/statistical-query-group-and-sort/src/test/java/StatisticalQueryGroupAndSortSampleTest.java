12e84d6ccd2bee893102a168765a2dfa1f75ce50
Tyler Schiewe <tyler.schiewe@gmail.com>
2019-08-27 09:04:34 -0700

    add initial tests

---
+++ feature_layers/statistical-query-group-and-sort/src/test/java/StatisticalQueryGroupAndSortSampleTest.java
@@ -0,0 +1,95 @@
+import com.esri.samples.statistical_query_group_and_sort.StatisticalQueryGroupAndSortSample;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.TableView;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class StatisticalQueryGroupAndSortSampleTest extends FxRobot {
+
+    private TableView statisticDefinitionTableView;
+    private ComboBox fieldNameComboBox;
+    private ComboBox statisticTypeComboBox;
+
+    @Before
+    public void setup() throws Exception {
+        ApplicationTest.launch(StatisticalQueryGroupAndSortSample.class);
+
+        statisticDefinitionTableView = lookup("#statisticDefinitionsTableView").queryTableView();
+        fieldNameComboBox = lookup("#fieldNameComboBox").queryComboBox();
+        statisticTypeComboBox = lookup("#statisticTypeComboBox").queryComboBox();
+
+        // wait for initialization
+        sleep(1000);
+    }
+
+    @After
+    public void cleanup() throws Exception {
+        FxToolkit.cleanupStages();
+    }
+
+    /**
+     * Tests adding a statistic definition to the table that is not already in the table. The number of items in the table should increment.
+     */
+    @Test
+    public void testAddNewFieldStatistic() {
+        int statisticsDefinitionsCount = statisticDefinitionTableView.getItems().size();
+
+        clickOn(fieldNameComboBox)
+                .type(KeyCode.UP)
+                .type(KeyCode.ENTER);
+
+        clickOn(statisticTypeComboBox)
+                .type(KeyCode.UP)
+                .type(KeyCode.ENTER);
+
+        clickOn("Add");
+
+        Assert.assertEquals(statisticDefinitionTableView.getItems().size(), statisticsDefinitionsCount + 1);
+    }
+
+    /**
+     * Tests adding a statistic definition to the table which is already in the table. A warning should display.
+     */
+    @Test
+    public void testAddExistingFieldStatistic() {
+        clickOn(statisticTypeComboBox)
+                .type(KeyCode.DOWN)
+                .type(KeyCode.ENTER);
+
+        clickOn("Add");
+
+        // create list to reverse from unmodifiable list
+        final List<Window> windows = new ArrayList<>(robotContext().getWindowFinder().listWindows());
+        // order top to bottom
+        Collections.reverse(windows);
+
+        Assert.assertEquals(windows.size(), 2);
+
+        Stage alertStage = (Stage) windows.stream()
+                .filter(window -> window instanceof Stage)
+                .filter(window -> ((Stage) window).getModality() == Modality.APPLICATION_MODAL)
+                .findFirst()
+                .orElse(null);
+
+        Assert.assertNotNull(alertStage);
+
+        final DialogPane alertDialogPane = (DialogPane) alertStage.getScene().getRoot();
+        Assert.assertEquals(alertDialogPane.getContentText(), "The selected combination has already been chosen.");
+    }
+
+
+}
---
+++ map/min-max-scale/src/test/java/MinMaxScaleSampleTest.java
@@ -0,0 +1,57 @@
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.samples.min_max_scale.MinMaxScaleSample;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
+
+public class MinMaxScaleSampleTest extends FxRobot {
+
+    private MapView mapView;
+
+    @Before
+    public void setup() throws Exception {
+        ApplicationTest.launch(MinMaxScaleSample.class);
+        mapView = lookup(n -> n instanceof MapView).query();
+    }
+
+    @After
+    public void cleanup() throws Exception {
+        FxToolkit.cleanupStages();
+    }
+
+    /**
+     * Tests adding a statistic definition to the table that is not already in the table. The number of items in the table should increment.
+     */
+    @Test
+    public void testScrollBoundByScale() {
+        ArcGISMap map = mapView.getMap();
+
+        double minScale = map.getMinScale();
+        double maxScale = map.getMaxScale();
+
+        moveTo(mapView);
+
+        sleep(1000);
+
+        while(mapView.getMapScale() < minScale) {
+            robotContext().getMouseRobot().scroll(2);
+            sleep(200);
+        }
+
+        Assert.assertEquals(mapView.getMapScale(), minScale, 5);
+
+        while(mapView.getMapScale() > maxScale) {
+            robotContext().getMouseRobot().scroll(-2);
+            sleep(200);
+        }
+
+        Assert.assertEquals(mapView.getMapScale(), maxScale, 5);
+    }
+
+
+}
