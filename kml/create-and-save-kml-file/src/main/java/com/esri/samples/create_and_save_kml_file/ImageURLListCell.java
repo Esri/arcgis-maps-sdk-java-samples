a0f2be83893b35d58cb67be994d26641d76e9e44
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-08-28 22:00:07 +0100

    Create and save kml file (#406)
    * create folder structure, add readme, metadata.json
    
    * split to mvc and add fxml file
    
    * ui and button events
    
    * UI and sketch mode
    
    * add basic kml sketching
    
    * show and hide style window after sketch complete
    
    * UI toggling based on app state
    
    * refactoring
    
    * add applying style
    
    * add saving
    
    * add combobox item list cell
    
    * show preview in combobox
    
    * JavaDoc comments
    
    * comments and refactoring
    
    * UI improvements
    
    * copyright in list cell factory
    
    * Center labels in UI
    
    * update README.md
    
    * add screenshot
    
    * improve saving
    
    * tidy ui, wait for map to load to enable ui
    
    * formatting
    
    * formatting fxml
    
    * optmise imports
    
    * remove unused css classes
    
    * improve file saving
    
    * refactor sample and improve UI
    
    * refactoring and comments
    
    * simplify UI (#407)
    
    * simplify UI
    
    * reuse file chooser
    
    * update readme, metadata, and image
    
    * add comments
    
    * remove unused imports

---
+++ kml/create-and-save-kml-file/src/main/java/com/esri/samples/create_and_save_kml_file/ImageURLListCell.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019 Esri.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.esri.samples.create_and_save_kml_file;
+
+import javafx.scene.control.ListCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ * Shows an image based on the URL String value.
+ */
+public class ImageURLListCell extends ListCell<String> {
+
+  @Override
+  protected void updateItem(String item, boolean empty) {
+    super.updateItem(item, empty);
+
+    ImageView imageView = new ImageView();
+
+    if (item == null || empty) {
+      // show 'Default' text instead of an image when the String is null
+      setGraphic(null);
+      setText("Default");
+    } else {
+      // display image from URL
+      imageView.setImage(new Image(item, 0, 15, true, true));
+      setGraphic(imageView);
+      setText(null);
+    }
+  }
+}
