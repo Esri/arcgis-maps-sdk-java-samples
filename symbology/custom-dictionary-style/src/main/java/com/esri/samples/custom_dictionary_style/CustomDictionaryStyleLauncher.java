5e0f94e08527c802e2cdfbf294ab80d38796d98d
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-09-04 12:33:32 +0100

    add sample

---
+++ symbology/custom-dictionary-style/src/main/java/com/esri/samples/custom_dictionary_style/CustomDictionaryStyleLauncher.java
@@ -0,0 +1,11 @@
+package com.esri.samples.custom_dictionary_style;
+
+/**
+ * Wrapper required for launching a JavaFX 11 app through Gradle or from a jar.
+ */
+public class CustomDictionaryStyleLauncher {
+
+  public static void main(String[] args) {
+    CustomDictionaryStyleSample.main(args);
+  }
+}
