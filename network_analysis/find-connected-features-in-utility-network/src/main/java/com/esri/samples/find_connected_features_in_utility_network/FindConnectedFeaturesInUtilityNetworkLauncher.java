b4cf8dbb549aa8e9b4bb372976a9bd17a647dd13
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-09-16 09:35:05 +0100

    use GeometryEngine.nearestCoordinate(), javadocs, formatting

--- network_analysis/find-connected-features-in-utility-network/src/main/java/com/esri/samples/find_connected_features_in_utility_network/FindConnectedFeaturesInUtilityNetworkLauncher.java
+++ network_analysis/find-connected-features-in-utility-network/src/main/java/com/esri/samples/find_connected_features_in_utility_network/FindConnectedFeaturesInUtilityNetworkLauncher.java
@@ -1,8 +1,8 @@
 package com.esri.samples.find_connected_features_in_utility_network;
 
 /**
-* Wrapper required for launching a JavaFX 11 app through Gradle or from a jar.
-*/
+ * Wrapper required for launching a JavaFX 11 app through Gradle or from a jar.
+ */
 public class FindConnectedFeaturesInUtilityNetworkLauncher {
 
   public static void main(String[] args) {
