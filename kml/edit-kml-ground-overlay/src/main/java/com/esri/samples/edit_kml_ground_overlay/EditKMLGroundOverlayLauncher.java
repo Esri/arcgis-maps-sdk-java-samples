f19a1d7b85061596200c0f3ac0ada4f30e4cb8f8
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-10-24 10:17:43 +0100

    New Sample - Edit kml ground overlay (#409)
    * initial commit
    
    * fix comments
    
    * refactoring
    
    * comments
    
    * fix URI
    
    * remove unused CSS, add screenshot
    
    * implement review corrections
    
    * update screenshot for new UI
    
    * UI changes, simplify applying opacity
    
    * setGroundOverlayOpacity() comment wording
    
    * update scene comment for clarity
    
    * review corrections
    
    * Change fields to local variables

---
+++ kml/edit-kml-ground-overlay/src/main/java/com/esri/samples/edit_kml_ground_overlay/EditKMLGroundOverlayLauncher.java
@@ -0,0 +1,11 @@
+package com.esri.samples.edit_kml_ground_overlay;
+
+/**
+ * Wrapper required for launching a JavaFX 11 app through Gradle or from a jar.
+ */
+
+public class EditKMLGroundOverlayLauncher {
+  public static void main(String[] args) {
+    EditKMLGroundOverlaySample.main(args);
+  }
+}
