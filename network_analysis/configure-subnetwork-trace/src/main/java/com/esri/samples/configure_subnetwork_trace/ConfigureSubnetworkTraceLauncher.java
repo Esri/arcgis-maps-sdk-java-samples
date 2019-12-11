9a20fd1f24f60f1b1c1abab3a960ddca198b123a
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-12-11 10:18:29 +0000

    add base structure

---
+++ network_analysis/configure-subnetwork-trace/src/main/java/com/esri/samples/configure_subnetwork_trace/ConfigureSubnetworkTraceLauncher.java
@@ -0,0 +1,11 @@
+package com.esri.samples.configure_subnetwork_trace;
+
+/**
+ * Wrapper required for launching a JavaFX 11 app through Gradle or from a jar.
+ */
+public class ConfigureSubnetworkTraceLauncher {
+
+  public static void main(String[] args) {
+    ConfigureSubnetworkTraceSample.main(args);
+  }
+}
