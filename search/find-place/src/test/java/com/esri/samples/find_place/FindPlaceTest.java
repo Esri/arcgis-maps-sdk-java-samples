239498757607c25541ca96f42a1460b5598b479b
Jonathan Lavi <jonathan.lavi@gmail.com>
2020-01-09 17:07:01 +0000

    click on starbucks, click on search

--- search/find-place/src/test/java/com/esri/samples/find_place/FindPlaceTest.java
+++ search/find-place/src/test/java/com/esri/samples/find_place/FindPlaceTest.java
@@ -1,14 +1,18 @@
 package com.esri.samples.find_place;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.loadui.testfx.GuiTest;
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 
@@ -36,8 +40,20 @@ public class FindPlaceTest extends ApplicationTest {
   }
 
   @Test
-  public void testClickButton () {
-    clickOn("#searchButton");
-    write("This is a test!");
+  public void testClickButton () throws InterruptedException {
+
+    ComboBox<String> placeBox = lookup("#placeBox").query();
+
+    // Find and click only on arrow button. This is important for editable combo-boxes.
+    for (Node child : placeBox.getChildrenUnmodifiable()) {
+      if (child.getStyleClass().contains("arrow-button")) {
+        Node arrowRegion = ((Pane) child).getChildren().get(0);
+        clickOn(arrowRegion);
+        Thread.sleep(100); // try/catch were skipped for shorter code.
+        clickOn("Starbucks");
+      }
+    }
+
+    clickOn("Search");
   }
 }
