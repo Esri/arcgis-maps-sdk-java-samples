b29b5b4cedabae9130075d5cf35a76d45121114b
Jonathan Lavi <jonathan.lavi@gmail.com>
2020-01-10 12:17:59 +0000

    tidy and refactor

--- search/find-place/src/test/java/com/esri/samples/find_place/FindPlaceTest.java
+++ search/find-place/src/test/java/com/esri/samples/find_place/FindPlaceTest.java
@@ -1,59 +1,119 @@
 package com.esri.samples.find_place;
 
-import javafx.fxml.FXMLLoader;
+import com.esri.arcgisruntime.mapping.view.DrawStatus;
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
+import com.esri.arcgisruntime.mapping.view.MapView;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseButton;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.loadui.testfx.GuiTest;
+import org.testfx.api.FxRobot;
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-public class FindPlaceTest extends ApplicationTest {
+public class FindPlaceTest extends FxRobot {
 
-  @Override
-  public void start (Stage stage) throws Exception {
-    Parent mainNode = FXMLLoader.load(FindPlaceSample.class.getResource("/find_place.fxml"));
-    stage.setScene(new Scene(mainNode));
-    stage.show();
-    stage.toFront();
-  }
+  Object mapView;
+  ComboBox<String> placeBox;
+  ComboBox<String> locationBox;
+  Node locationBoxArrowRegion;
+  Node placeBoxArrowRegion;
 
   @Before
-  public void setUp () throws Exception {
-  }
+  public void setup() throws Exception {
+    // launch the sample
+    ApplicationTest.launch(FindPlaceSample.class);
 
-  @After
-  public void tearDown () throws Exception {
-    FxToolkit.hideStage();
-    release(new KeyCode[]{});
-    release(new MouseButton[]{});
-  }
+    // get a handle on the MapView
+    Node mapViewNode = lookup("#mapView").query();
+    if (mapViewNode instanceof MapView) {
+      mapView = mapViewNode;
+    } else {
+      fail("MapView Node could not be found");
+    }
 
-  @Test
-  public void testClickButton () throws InterruptedException {
+    // get a handle on the ComboBoxes
+    placeBox = lookup("#placeBox").queryComboBox();
+    locationBox = lookup("#locationBox").queryComboBox();
 
-    ComboBox<String> placeBox = lookup("#placeBox").query();
-
-    // Find and click only on arrow button. This is important for editable combo-boxes.
-    for (Node child : placeBox.getChildrenUnmodifiable()) {
+    // get a handle on the ComboBoxes' arrow buttons
+    for (Node child : locationBox.getChildrenUnmodifiable()) {
       if (child.getStyleClass().contains("arrow-button")) {
-        Node arrowRegion = ((Pane) child).getChildren().get(0);
-        clickOn(arrowRegion);
-        Thread.sleep(100); // try/catch were skipped for shorter code.
-        clickOn("Starbucks");
+        locationBoxArrowRegion = ((Pane) child).getChildren().get(0);
       }
     }
 
-    clickOn("Search");
+    for (Node child : placeBox.getChildrenUnmodifiable()) {
+      if (child.getStyleClass().contains("arrow-button")) {
+        placeBoxArrowRegion = ((Pane) child).getChildren().get(0);
+      }
+    }
+
+    // wait for initialization
+    sleep(1000);
   }
+
+  //  @Override
+  //  public void start(Stage stage) throws Exception {
+  //    Parent mainNode = FXMLLoader.load(FindPlaceSample.class.getResource("/find_place.fxml"));
+  //    stage.setScene(new Scene(mainNode));
+  //    stage.show();
+  //    stage.toFront();
+  //
+  //    // wait for initialization
+  //    sleep(1000);
+  //  }
+
+  @After
+  public void tearDown() throws Exception {
+    FxToolkit.cleanupStages();
+    // wait before the next test
+    sleep(1000);
+  }
+
+  /**
+   * Test Case 1: When using one of the pre-defined search inputs, the app returns the expected number of results
+   */
+  @Test
+  public void defaultSearchInput() {
+
+    clickOn(placeBoxArrowRegion);
+    sleep(500);
+    clickOn("Starbucks");
+    sleep(500);
+
+    clickOn(locationBoxArrowRegion);
+    sleep(500);
+    clickOn("Los Angeles, CA");
+    sleep(500);
+
+    clickOn("Search");
+
+    // wait for the map view to zoom to the location
+    if (mapView instanceof MapView) {
+      ((MapView) mapView).addDrawStatusChangedListener((drawStatusChangedEvent) -> {
+        if (drawStatusChangedEvent.getDrawStatus() == DrawStatus.COMPLETED) {
+          // get the graphics overlay and assert that the expected number of graphics (location pins) is displayed
+          GraphicsOverlay graphicsOverlay = ((MapView) mapView).getGraphicsOverlays().get(0);
+          assertEquals("Unexpected number of graphics (location pins) found",50, graphicsOverlay.getGraphics().size());
+        }
+      });
+    }
+
+    sleep(1000);
+  }
+
+
+
+  // test cases:
+  // TODO: find callout node in map view and confirm text (probably something you'll be doing in our unit tests)
+  // TODO: each result has a corresponding pin shown
+  // TODO: clicking on map triggers identify and callout
+  // TODO: panning map triggers prompt for new results
+
 }
