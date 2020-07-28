279eb0e61b2c0db648646cd27d31a965a423f8cf
sclaridge <67917108+sclaridge@users.noreply.github.com>
2020-07-28 17:41:34 +0100

    Set up feature linked annotation sample

---
+++ editing/edit-features-with-feature-linked-annotation/src/main/java/com.esri.samples.edit_features_with_feature_linked_annotation/EditFeaturesWithFeatureLinkedAnnotationLauncher.java
@@ -0,0 +1,11 @@
+package com.esri.samples.edit_features_with_feature_linked_annotation;
+
+/**
+ * Wrapper required for launching a JavaFX 11 app through Gradle or from a jar.
+ */
+public class EditFeaturesWithFeatureLinkedAnnotationLauncher {
+  public static void main(String[] args) {
+    EditFeaturesWithFeatureLinkedAnnotationSample.main(args);
+  }
+}
+
