583d941ae22c6b241ef98ac40e92b43b7603fbc4
Jen Merritt <56556822+jenmerritt@users.noreply.github.com>
2020-07-30 17:26:40 +0100

    functionality to show and hide layer with buttons

--- map_view/display-layer-view-state/src/main/java/com/esri/samples/display_layer_view_state/DisplayLayerViewStateSample.java
+++ map_view/display-layer-view-state/src/main/java/com/esri/samples/display_layer_view_state/DisplayLayerViewStateSample.java
@@ -20,6 +20,7 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -32,6 +33,7 @@ import javafx.stage.Stage;
 import com.esri.arcgisruntime.geometry.Point;
 import com.esri.arcgisruntime.geometry.SpatialReferences;
 import com.esri.arcgisruntime.layers.FeatureLayer;
+import com.esri.arcgisruntime.layers.Layer;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.Viewpoint;
@@ -42,7 +44,10 @@ import com.esri.arcgisruntime.portal.PortalItem;
 public class DisplayLayerViewStateSample extends Application {
 
   private MapView mapView;
+  private FeatureLayer featureLayer;
   private Label currentStatusLabel;
+  private Button loadButton;
+  private Button hideButton;
 
   @Override
   public void start(Stage stage) {
@@ -85,20 +90,15 @@ public class DisplayLayerViewStateSample extends Application {
       // add the label to the control panel
       controlsVBox.getChildren().add(currentStatusLabel);
 
-      // create a feature layer from a portal item
-      final PortalItem portalItem = new PortalItem(new Portal("https://runtime.maps.arcgis.com/"),
-        "b8f4033069f141729ffb298b7418b653");
-      final FeatureLayer featureLayer = new FeatureLayer(portalItem, 0);
-
-      //  set a minimum and maximum scale for the visibility of the feature layer
-      featureLayer.setMinScale(40000000);
-      featureLayer.setMaxScale(40000000 / 10);
-
-      // add the feature layer to the map to load it
-      map.getOperationalLayers().add(featureLayer);
-
       // create a listener that fires every time a layers' view status has changed
       mapView.addLayerViewStateChangedListener(statusChangeEvent -> {
+
+        // get the layer which state has changed
+        Layer layer = statusChangeEvent.getLayer();
+        // only update if the layer is the feature layer we're tracking
+        if (layer != featureLayer) {
+          return ;
+        }
 
         // get the layer view status
         String layerViewStatus = statusChangeEvent.getLayerViewStatus().iterator().next().toString();
@@ -111,9 +111,62 @@ public class DisplayLayerViewStateSample extends Application {
 
       });
 
+      VBox buttonsVBox = new VBox(6);
+      buttonsVBox.setBackground(new Background(new BackgroundFill(Paint.valueOf("rgba(0,0,0,0.3)"), CornerRadii.EMPTY,
+        Insets.EMPTY)));
+      buttonsVBox.setPadding(new Insets(10.0));
+      buttonsVBox.setMaxSize(150, 20);
+      buttonsVBox.getStyleClass().add("panel-region");
+      loadButton = new Button("Show Layer");
+      loadButton.setMaxWidth(130);
+      hideButton = new Button("Hide Layer");
+      hideButton.setMaxWidth(130);
+      buttonsVBox.getChildren().add(loadButton);
+
+      loadButton.setOnAction(event -> {
+
+        if (featureLayer != null && !featureLayer.isVisible()){
+          featureLayer.setVisible(true);
+        }
+        else {
+          // create a feature layer from a portal item
+          final PortalItem portalItem = new PortalItem(new Portal("https://runtime.maps.arcgis.com/"),
+            "b8f4033069f141729ffb298b7418b653");
+          featureLayer = new FeatureLayer(portalItem, 0);
+
+          //  set a minimum and maximum scale for the visibility of the feature layer
+          featureLayer.setMinScale(40000000);
+          featureLayer.setMaxScale(40000000 / 10);
+
+          // add the feature layer to the map to load it
+          map.getOperationalLayers().add(featureLayer);
+        }
+
+          // hide the load button
+          buttonsVBox.getChildren().clear();
+          buttonsVBox.getChildren().add(hideButton);
+      });
+
+      hideButton.setOnAction(event -> {
+        if (featureLayer == null) return;
+
+        if (featureLayer.isVisible()) {
+          featureLayer.setVisible(false);
+          buttonsVBox.getChildren().clear();
+          buttonsVBox.getChildren().add(loadButton);
+        }
+        else {
+          featureLayer.setVisible(true);
+          buttonsVBox.getChildren().clear();
+          buttonsVBox.getChildren().add(hideButton);
+        }
+      });
+
       // add the map view and control panel to the stack pane
-      stackPane.getChildren().addAll(mapView, controlsVBox);
+      stackPane.getChildren().addAll(mapView, controlsVBox, buttonsVBox);
       StackPane.setAlignment(controlsVBox, Pos.TOP_CENTER);
+      StackPane.setAlignment(buttonsVBox, Pos.TOP_LEFT);
+      StackPane.setMargin(buttonsVBox, new Insets(10, 0, 0, 10));
       StackPane.setMargin(controlsVBox, new Insets(10, 0, 0, 10));
     } catch (Exception e) {
       // on any error, display the stack trace
