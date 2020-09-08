00422655d30491086d4c5f58fdea42fe65a31146
sclaridge <67917108+sclaridge@users.noreply.github.com>
2020-09-08 11:41:13 +0100

    Add switch

--- raster/apply-mosaic-rule-to-rasters/src/main/java/com/esri/samples/apply_mosaic_rule_to_rasters/ApplyMosaicRuleToRastersSample.java
+++ raster/apply-mosaic-rule-to-rasters/src/main/java/com/esri/samples/apply_mosaic_rule_to_rasters/ApplyMosaicRuleToRastersSample.java
@@ -16,6 +16,8 @@
 
 package com.esri.samples.apply_mosaic_rule_to_rasters;
 
+import java.util.Arrays;
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -43,14 +45,10 @@ import com.esri.arcgisruntime.raster.MosaicRule;
 import com.esri.arcgisruntime.raster.MosaicMethod;
 import com.esri.arcgisruntime.raster.MosaicOperation;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class ApplyMosaicRuleToRastersSample extends Application {
 
   private MapView mapView;
   private ImageServiceRaster imageServiceRaster;
-  private ComboBox<String> comboBox;
   private MosaicRule mosaicRule;
 
   @Override
@@ -68,6 +66,25 @@ public class ApplyMosaicRuleToRastersSample extends Application {
       stage.setScene(scene);
       stage.show();
 
+      // set up the control panel UI
+      VBox controlsVBox = new VBox(6);
+      controlsVBox.setBackground(new Background(new BackgroundFill(Paint.valueOf("rgba(0, 0, 0, 0.3)"),
+              CornerRadii.EMPTY, Insets.EMPTY)));
+      controlsVBox.setPadding(new Insets(10.0));
+      controlsVBox.setMaxSize(260, 50);
+      controlsVBox.setVisible(false);
+
+      // create a label
+      Label mosaicRuleLabel = new Label("Choose a mosaic rule for the image service: ");
+      mosaicRuleLabel.setTextFill(Color.WHITE);
+
+      // create a combo box
+      ComboBox<String> comboBox = new ComboBox<>();
+      comboBox.setMaxWidth(Double.MAX_VALUE);
+
+      // add the label and combo box to the control panel
+      controlsVBox.getChildren().addAll(mosaicRuleLabel, comboBox);
+
       // create a map with the topographic vector basemap
       ArcGISMap map = new ArcGISMap(Basemap.createTopographicVector());
 
@@ -75,26 +92,6 @@ public class ApplyMosaicRuleToRastersSample extends Application {
       mapView = new MapView();
       mapView.setMap(map);
 
-      // set up the control panel UI
-      VBox controlsVBox = new VBox(6);
-      controlsVBox.setBackground(new Background(new BackgroundFill(Paint.valueOf("rgba(0, 0, 0, 0.3)"),
-        CornerRadii.EMPTY, Insets.EMPTY)));
-      controlsVBox.setPadding(new Insets(10.0));
-      controlsVBox.setMaxSize(260,50);
-      controlsVBox.setVisible(false);
-
-      // create a label
-      Label mosaicRuleLabel = new Label ("Choose a mosaic rule for the image service: ");
-      mosaicRuleLabel.setTextFill(Color.WHITE);
-
-      // create a combo box
-      comboBox = new ComboBox<>();
-      comboBox.setMaxWidth(Double.MAX_VALUE);
-
-      // add the label and combo box to the control panel
-      controlsVBox.getChildren().addAll(mosaicRuleLabel, comboBox);
-
-      // create an image service raster from a url for an image service
       imageServiceRaster = new ImageServiceRaster(imageServiceURL);
 
       // create a raster layer from the image service raster
@@ -119,14 +116,41 @@ public class ApplyMosaicRuleToRastersSample extends Application {
           // set the default combo box value
           comboBox.setValue("Default");
 
-          // check if a mosaic rule exists. If not, create one
+          // create a mosaic rule for the image service raster if one does not already exist
           if (imageServiceRaster.getMosaicRule() == null) {
             mosaicRule = new MosaicRule();
             imageServiceRaster.setMosaicRule(mosaicRule);
           }
-          // set the mosaic rule based on rule chosen from the combo box
+
+          // set the mosaic rule of the image service raster based on rule chosen from the combo box
           comboBox.getSelectionModel().selectedItemProperty().addListener(e -> {
-            setMosaicRule(comboBox.getSelectionModel().getSelectedItem());
+            // create a new mosaic rule
+            mosaicRule = new MosaicRule();
+
+            switch (comboBox.getSelectionModel().getSelectedItem()) {
+              case "Default":
+                mosaicRule.setMosaicMethod(MosaicMethod.NONE);
+                break;
+              case "Northwest":
+                mosaicRule.setMosaicMethod(MosaicMethod.NORTHWEST);
+                mosaicRule.setMosaicOperation(MosaicOperation.FIRST);
+                break;
+              case "Center":
+                mosaicRule.setMosaicMethod(MosaicMethod.CENTER);
+                mosaicRule.setMosaicOperation(MosaicOperation.BLEND);
+                break;
+              case "By attribute":
+                mosaicRule.setMosaicMethod(MosaicMethod.ATTRIBUTE);
+                mosaicRule.setSortField("OBJECTID");
+                break;
+              case "Lock raster":
+                mosaicRule.setMosaicMethod(MosaicMethod.LOCK_RASTER);
+                mosaicRule.getLockRasterIds().clear();
+                mosaicRule.getLockRasterIds().addAll(Arrays.asList(1L, 7L, 12L));
+                break;
+            }
+            // set the mosaic rule of the image service raster
+            imageServiceRaster.setMosaicRule(mosaicRule);
           });
         } else {
           // show alert if raster layer fails to load.
@@ -136,49 +160,13 @@ public class ApplyMosaicRuleToRastersSample extends Application {
 
       // add the map view and the control panel to the stack pane
       stackPane.getChildren().addAll(mapView, controlsVBox);
-      StackPane.setAlignment(controlsVBox, Pos.TOP_RIGHT);
+      StackPane.setAlignment(controlsVBox, Pos.TOP_LEFT);
       StackPane.setMargin(controlsVBox, new Insets(10, 0, 0, 10));
 
     } catch (Exception e) {
       // on any error, display the stack trace
       e.printStackTrace();
     }
-  }
-
-  /**
-   * Applies one of the predefined mosaic rules to the image service raster.
-   *
-   * @param mosaicMethod one of "Default", "Northwest", "Center", "By attribute", and "Lock raster"
-   */
-  private void setMosaicRule(String mosaicMethod) {
-
-    if (mosaicMethod == "Default") {
-      mosaicRule = new MosaicRule();
-      mosaicRule.setMosaicMethod(MosaicMethod.NONE);
-    }
-
-    if (mosaicMethod == "Northwest") {
-      mosaicRule = new MosaicRule();
-      mosaicRule.setMosaicMethod(MosaicMethod.NORTHWEST);
-      mosaicRule.setMosaicOperation(MosaicOperation.FIRST);
-    }
-    if (mosaicMethod == "Center") {
-      mosaicRule = new MosaicRule();
-      mosaicRule.setMosaicMethod(MosaicMethod.CENTER);
-      mosaicRule.setMosaicOperation(MosaicOperation.BLEND);
-    }
-    if (mosaicMethod == "By attribute") {
-      mosaicRule = new MosaicRule();
-      mosaicRule.setMosaicMethod(MosaicMethod.ATTRIBUTE);
-      mosaicRule.setSortField("OBJECTID");
-    }
-    if (mosaicMethod == "Lock raster") {
-      mosaicRule = new MosaicRule();
-      mosaicRule.setMosaicMethod(MosaicMethod.LOCK_RASTER);
-      mosaicRule.getLockRasterIds().clear();
-      mosaicRule.getLockRasterIds().addAll(Arrays.asList(Long.valueOf(1), Long.valueOf(7), Long.valueOf(12)));
-    }
-    imageServiceRaster.setMosaicRule(mosaicRule);
   }
 
   /**
