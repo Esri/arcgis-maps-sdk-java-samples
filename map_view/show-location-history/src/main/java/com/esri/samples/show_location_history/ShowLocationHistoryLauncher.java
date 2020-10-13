d8e952860265fd0af4f3d921cad77d6bbdf35cab
Simone Claridge <sclaridge@esri.com>
2020-10-13 09:56:52 +0100

    New sample: Show location history (#556)
    * Set up sample
    
    * Update readme and add polyline data
    
    * Create graphic overlays for lines and points
    
    * Add location tracking
    
    * Update README.metadata.json
    
    * Add screenshot
    
    * Update comments
    
    * Update ShowLocationHistory.png
    
    * Add newline to json
    
    * Add changes from PR
    
    * Remove try catch from loading listener
    
    * Refactor code from PR
    
    * Remove unused import
    
    * Fix README
    
    * Add location changed listener
    
    * Fix listener comment
    
    * Remove try catch, add toggle button
    
    * Update README.md
    
    * Add highlighted button, gif
    
    * Remove Hbox spacing
    
    * Fix comments from PR

---
+++ map_view/show-location-history/src/main/java/com/esri/samples/show_location_history/ShowLocationHistoryLauncher.java
@@ -0,0 +1,8 @@
+package com.esri.samples.show_location_history;
+
+/**
+ * Wrapper required for launching a JavaFX 11 app through Gradle or from a jar.
+ */
+public class ShowLocationHistoryLauncher {
+  public static void main(String[] args) { ShowLocationHistorySample.main(args); }
+}
