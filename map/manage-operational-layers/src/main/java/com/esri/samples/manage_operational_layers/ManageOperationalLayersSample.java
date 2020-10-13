ec04025947b998aa78ddf2f479127d10e255ef5e
Jen Merritt <jmerritt@esri.com>
2020-10-13 09:40:52 +0100

    Sync dev with main (#569)
    * List related features sample: use correct identify workflow (#555)
    
    * update method for identify workflow
    
    * update for loop
    
    * New sample: Perform valve isolation trace (#449)
    
    * update http to https for world geocoding service (#561)
    
    * Small fixes to samples from testing (#563)
    
    * New sample: Configure subnetwork trace (#439)
    
    * Fix: Perform Valve Isolation Trace - increase trace button width (#564)
    
    * Perform Valve Isolation Trace: update README.metadata.json (#567)
    
    * Manage Operational Layers: UI not working as described (#562)
    
    * alter the functionality for reordering layers
    * code review amends
    * change initial viewpoint location
    * creation of image layers listener
    * comment correction
    
    * New Sample: Edit with branch versioning (#553)
    
    * setup file structure
    
    * load feature layer
    
    * create and switch versions
    
    * ui layout, update and switch
    
    * update geometry functionality
    
    * refactor methods
    
    * tidy up spacing and comments
    
    * update readme and add screenshot
    
    * update metadata file with script
    
    * correction to metadata file
    
    * tidy ups from code review
    
    * tidy ups
    
    * code review changes
    
    * refactors from review
    
    * tidy ups
    
    * small fixes from code review
    
    * create error alert method
    
    * create method for text validation
    
    * updates to readme
    
    * add link to docs in readme
    
    * tidy ups from code review
    
    * amend error messages
    
    * validation error messaging

--- map/manage-operational-layers/src/main/java/com/esri/samples/manage_operational_layers/ManageOperationalLayersSample.java
+++ map/manage-operational-layers/src/main/java/com/esri/samples/manage_operational_layers/ManageOperationalLayersSample.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2017 Esri.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -17,11 +17,13 @@
 package com.esri.samples.manage_operational_layers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseButton;
@@ -35,134 +37,156 @@ import javafx.stage.Stage;
 
 import com.esri.arcgisruntime.layers.ArcGISMapImageLayer;
 import com.esri.arcgisruntime.layers.Layer;
+import com.esri.arcgisruntime.loadable.LoadStatus;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.LayerList;
+import com.esri.arcgisruntime.mapping.Viewpoint;
 import com.esri.arcgisruntime.mapping.view.MapView;
 
 public class ManageOperationalLayersSample extends Application {
 
   private MapView mapView;
-  private LayerList mapAddedLayers;
-
-  private static final String ELEVATION_LAYER =
-      "https://sampleserver5.arcgisonline.com/arcgis/rest/services/Elevation/WorldElevations/MapServer";
-  private static final String CENSUS_LAYER =
-      "https://sampleserver5.arcgisonline.com/arcgis/rest/services/Census/MapServer";
-  private static final String DAMAGE_LAYER =
-      "https://sampleserver5.arcgisonline.com/arcgis/rest/services/DamageAssessment/MapServer";
 
   @Override
   public void start(Stage stage) {
 
     try {
-      // create stack pane and application scene
+      // create the stack pane and the application scene
       StackPane stackPane = new StackPane();
       Scene scene = new Scene(stackPane);
       scene.getStylesheets().add(getClass().getResource("/manage_operational_layers/style.css").toExternalForm());
 
-      // set title, size, and add scene to stage
+      // set a title, size, and add the scene to the stage
       stage.setTitle("Manage Operational Layers Sample");
       stage.setWidth(800);
       stage.setHeight(700);
       stage.setScene(scene);
       stage.show();
 
-      // create a control panel
-      VBox controlsVBox = new VBox(6);
-      controlsVBox.setBackground(new Background(new BackgroundFill(Paint.valueOf("rgba(0,0,0,0.3)"), CornerRadii.EMPTY,
-          Insets.EMPTY)));
-      controlsVBox.setPadding(new Insets(10.0));
-      controlsVBox.setMaxSize(200, 260);
-      controlsVBox.getStyleClass().add("panel-region");
+      // create an ArcGISMap with the topographic basemap
+      ArcGISMap map = new ArcGISMap(Basemap.createTopographic());
 
-      // create labels for add/delete layers
-      Label addLayersLabel = new Label("Layers on the ArcGISMap");
-      Label deleteLayersLabel = new Label("Deleted Layers");
-      addLayersLabel.getStyleClass().add("panel-label");
-      deleteLayersLabel.getStyleClass().add("panel-label");
-
-      // create list for deleted layers
-      ArrayList<Layer> deletedLayers = new ArrayList<>();
-
-      // create a list view for names of layers added/deleted
-      ListView<String> addedLayerNames = new ListView<>();
-      ListView<String> deletedLayerNames = new ListView<>();
-
-      addedLayerNames.setOnMouseClicked(e -> {
-        // index of selected item
-        int selectedIndex = addedLayerNames.getSelectionModel().getSelectedIndex();
-        if (e.getButton() == MouseButton.PRIMARY) {
-
-          // store selected layer
-          ArcGISMapImageLayer temp = (ArcGISMapImageLayer) mapAddedLayers.get(selectedIndex);
-          // remove selected layer
-          mapAddedLayers.remove(selectedIndex);
-          // add selected layer to front
-          mapAddedLayers.add(temp);
-
-          // make names match added layers list
-          String temp2 = addedLayerNames.getItems().get(selectedIndex);
-          addedLayerNames.getItems().remove(selectedIndex);
-          addedLayerNames.getItems().add(temp2);
-        } else if (e.getButton() == MouseButton.SECONDARY) {
-
-          // append added layer to deleted list
-          deletedLayers.add(mapAddedLayers.get(selectedIndex));
-          // remove layer from added list
-          mapAddedLayers.remove(selectedIndex);
-
-          // make names match corresponding list
-          deletedLayerNames.getItems().add(addedLayerNames.getSelectionModel().getSelectedItem());
-          addedLayerNames.getItems().remove(selectedIndex);
-        }
-      });
-
-      deletedLayerNames.setOnMouseClicked(e -> {
-        // index of selected item
-        int selectedIndex = deletedLayerNames.getSelectionModel().getSelectedIndex();
-        if (e.getButton() == MouseButton.SECONDARY) {
-
-          // append deleted layer to added list
-          mapAddedLayers.add(deletedLayers.get(selectedIndex));
-          // remove delete layer from deleted list
-          deletedLayers.remove(selectedIndex);
-
-          // make names match corresponding list
-          addedLayerNames.getItems().add(deletedLayerNames.getSelectionModel().getSelectedItem());
-          deletedLayerNames.getItems().remove(selectedIndex);
-        }
-      });
-
-      // add labels and lists to the control panel
-      controlsVBox.getChildren().addAll(addLayersLabel, addedLayerNames, deleteLayersLabel, deletedLayerNames);
-
-      // create a ArcGISMap with the basemap Topographic
-      final ArcGISMap map = new ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 14);
-
-      // get the operational layers list from the ArcGISMap
-      mapAddedLayers = map.getOperationalLayers();
-
-      // create a view and added ArcGISMap to it
+      // create a view and add the ArcGISMap to it
       mapView = new MapView();
       mapView.setMap(map);
 
-      // create the elevation, census, and damage default layers
-      ArcGISMapImageLayer imageLayerElevation = new ArcGISMapImageLayer(ELEVATION_LAYER);
-      ArcGISMapImageLayer imageLayerCensus = new ArcGISMapImageLayer(CENSUS_LAYER);
-      ArcGISMapImageLayer imageLayerDamage = new ArcGISMapImageLayer(DAMAGE_LAYER);
+      // set the initial viewpoint for the map view
+      mapView.setViewpoint(new Viewpoint(41.056295, -106.195800, 20000000));
 
-      // add default ArcGISMap image layers to the ArcGISMap
-      mapAddedLayers.add(imageLayerElevation);
-      mapAddedLayers.add(imageLayerCensus);
-      mapAddedLayers.add(imageLayerDamage);
+      // create and add new image layers to the map's operational layers
+      LayerList mapLayers = map.getOperationalLayers();
+      mapLayers.addAll(Arrays.asList(
+        (new ArcGISMapImageLayer("https://sampleserver5.arcgisonline.com/arcgis/rest/services/Elevation/WorldElevations/MapServer")),
+        (new ArcGISMapImageLayer("https://sampleserver5.arcgisonline.com/arcgis/rest/services/DamageAssessment/MapServer")),
+        (new ArcGISMapImageLayer("https://sampleserver5.arcgisonline.com/arcgis/rest/services/Census/MapServer"))
+      ));
 
-      // add the default names to added list
-      addedLayerNames.getItems().add(mapAddedLayers.get(0).getName());
-      addedLayerNames.getItems().add(mapAddedLayers.get(1).getName());
-      addedLayerNames.getItems().add(mapAddedLayers.get(2).getName());
+      // check that each layer has loaded correctly and if not display an error message
+      mapLayers.forEach(layer ->
+        layer.addDoneLoadingListener(() -> {
+          if (layer.getLoadStatus() != LoadStatus.LOADED) {
+            new Alert(Alert.AlertType.ERROR, "Map image layer failed to load: " + layer.getLoadError().getCause().getMessage()).show();
+          }
+        })
+      );
 
-      // add the map view and control box to stack pane
+      // create a list to hold deleted layers
+      ArrayList<Layer> deletedLayers = new ArrayList<>();
+
+      // create a list view to display the list of added and deleted layers by name, and create labels for them
+      ListView<String> addedLayerNamesList = new ListView<>();
+      Label addedLayersLabel = new Label("Added layers");
+      ListView<String> deletedLayerNamesList = new ListView<>();
+      Label deletedLayersLabel = new Label("Deleted layers");
+
+      // add the names of the layers on the map to the list of added layers
+      // note this should be the reverse order they were added to the map
+      addedLayerNamesList.getItems().add(mapLayers.get(2).getName());
+      addedLayerNamesList.getItems().add(mapLayers.get(1).getName());
+      addedLayerNamesList.getItems().add(mapLayers.get(0).getName());
+
+      // create a listener for clicks on the list of added layers
+      addedLayerNamesList.setOnMouseClicked(e -> {
+
+        // if there are added layers
+        if (!mapLayers.isEmpty() && !addedLayerNamesList.getItems().isEmpty()) {
+
+          // get the index of the selected item
+          int selectedIndex = addedLayerNamesList.getSelectionModel().getSelectedIndex();
+
+          // if a valid index is selected
+          if (selectedIndex >= 0) {
+
+            // get the selected layer from the map and its index in the list of operational layers on the map
+            ArcGISMapImageLayer selectedLayer = (ArcGISMapImageLayer) mapLayers.get(mapLayers.size() - selectedIndex - 1);
+            int indexOfSelectedLayer = mapLayers.indexOf(selectedLayer);
+
+            if (e.getButton() == MouseButton.PRIMARY) {
+
+              // if the layer is not already at the top, move it to the top of the list and layers on the map
+              if (selectedIndex != 0) {
+
+                // move the layer's name to the top of the added layers list. Note this is adding to index 0.
+                addedLayerNamesList.getItems().remove(selectedIndex);
+                addedLayerNamesList.getItems().add(0, selectedLayer.getName());
+
+                // move the layer to the top of the layers on the map. Note this is adding to the last index.
+                mapLayers.remove(indexOfSelectedLayer);
+                mapLayers.add(mapLayers.size(), selectedLayer);
+
+                // update the UI to keep the current layer selected
+                addedLayerNamesList.getSelectionModel().select(0);
+              }
+
+            } else if (e.getButton() == MouseButton.SECONDARY) {
+
+              // remove the selected layer from the map and add it to the deleted layers
+              mapLayers.remove(selectedLayer);
+              deletedLayers.add(selectedLayer);
+
+              // remove the selected layer's name from the added layers list and add it to the deleted layers list
+              addedLayerNamesList.getItems().remove(selectedIndex);
+              deletedLayerNamesList.getItems().add(selectedLayer.getName());
+            }
+          }
+        }
+      });
+
+      // create a listener for clicks on the list of deleted layers
+      deletedLayerNamesList.setOnMouseClicked(e -> {
+
+        // if there are deleted layers
+        if (!deletedLayers.isEmpty() && !deletedLayerNamesList.getItems().isEmpty()) {
+
+          // get the index of the selected layer
+          int selectedIndex = deletedLayerNamesList.getSelectionModel().getSelectedIndex();
+
+          // if a valid index is selected
+          if (selectedIndex >= 0) {
+
+            if (e.getButton() == MouseButton.SECONDARY) {
+
+              // add the layer to the map and remove from the deleted layers
+              mapLayers.add(0, deletedLayers.get(selectedIndex));
+              deletedLayers.remove(selectedIndex);
+
+              // add the selected layer's name to the added layers list and remove it from the deleted layers list
+              addedLayerNamesList.getItems().add(deletedLayerNamesList.getSelectionModel().getSelectedItem());
+              deletedLayerNamesList.getItems().remove(selectedIndex);
+            }
+          }
+        }
+      });
+
+      // create a control panel and add the label and list UI components
+      VBox controlsVBox = new VBox(6);
+      controlsVBox.setBackground(new Background(new BackgroundFill(Paint.valueOf("rgba(0,0,0,0.3)"), CornerRadii.EMPTY,
+        Insets.EMPTY)));
+      controlsVBox.getStyleClass().add("panel-region");
+      controlsVBox.getChildren().addAll(addedLayersLabel, addedLayerNamesList, deletedLayersLabel, deletedLayerNamesList);
+
+      // add the map view and control panel to stack pane
       stackPane.getChildren().addAll(mapView, controlsVBox);
       StackPane.setAlignment(controlsVBox, Pos.TOP_LEFT);
       StackPane.setMargin(controlsVBox, new Insets(10, 0, 0, 10));
@@ -192,5 +216,4 @@ public class ManageOperationalLayersSample extends Application {
 
     Application.launch(args);
   }
-
 }
