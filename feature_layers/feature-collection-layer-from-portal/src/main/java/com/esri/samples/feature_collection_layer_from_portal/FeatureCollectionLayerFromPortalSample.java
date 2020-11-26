d7547d86adcdf6352718c441872626b64b36c668
Jen Merritt <56556822+jenmerritt@users.noreply.github.com>
2020-11-26 15:02:04 +0000

    Feature collection layer from portal - fix indentations

--- feature_layers/feature-collection-layer-from-portal/src/main/java/com/esri/samples/feature_collection_layer_from_portal/FeatureCollectionLayerFromPortalSample.java
+++ feature_layers/feature-collection-layer-from-portal/src/main/java/com/esri/samples/feature_collection_layer_from_portal/FeatureCollectionLayerFromPortalSample.java
@@ -38,122 +38,122 @@ import com.esri.arcgisruntime.portal.PortalItem;
 
 public class FeatureCollectionLayerFromPortalSample extends Application {
 
-    private ArcGISMap map;
-    private MapView mapView;
-    private Portal portal;
-    private TextField inputTextField;
-    private PortalItem portalItem; // keep loadable in scope to avoid garbage collection
+  private ArcGISMap map;
+  private MapView mapView;
+  private Portal portal;
+  private TextField inputTextField;
+  private PortalItem portalItem; // keep loadable in scope to avoid garbage collection
 
-    @Override
-    public void start(Stage stage) {
-        // set up the application scene
-        try {
-            // create stack pane and application scene
-            StackPane stackPane = new StackPane();
-            Scene scene = new Scene(stackPane);
+  @Override
+  public void start(Stage stage) {
+    // set up the application scene
+    try {
+      // create stack pane and application scene
+      StackPane stackPane = new StackPane();
+      Scene scene = new Scene(stackPane);
 
-            // set title, size, and add scene to stage
-            stage.setTitle("Feature Collection Layer From Portal");
-            stage.setWidth(800);
-            stage.setHeight(700);
-            stage.setScene(scene);
-            stage.show();
+      // set title, size, and add scene to stage
+      stage.setTitle("Feature Collection Layer From Portal");
+      stage.setWidth(800);
+      stage.setHeight(700);
+      stage.setScene(scene);
+      stage.show();
 
-            // authentication with an API key or named user is required to access basemaps and other location services
-            String yourAPIKey = System.getProperty("apiKey");
-            ArcGISRuntimeEnvironment.setApiKey(yourAPIKey);
+      // authentication with an API key or named user is required to access basemaps and other location services
+      String yourAPIKey = System.getProperty("apiKey");
+      ArcGISRuntimeEnvironment.setApiKey(yourAPIKey);
 
-            // create a map with the oceans basemap style
-            map = new ArcGISMap(BasemapStyle.ARCGIS_OCEANS);
+      // create a map with the oceans basemap style
+      map = new ArcGISMap(BasemapStyle.ARCGIS_OCEANS);
 
-            // create a map view and set the map to it
-            mapView = new MapView();
-            mapView.setMap(map);
+      // create a map view and set the map to it
+      mapView = new MapView();
+      mapView.setMap(map);
 
-            // create portal
-            portal = new Portal("https://www.arcgis.com/");
+      // create portal
+      portal = new Portal("https://www.arcgis.com/");
 
-            // create text field to input user's own portal item ID
-            inputTextField = new TextField("32798dfad17942858d5eef82ee802f0b");
-            inputTextField.setMaxWidth(250);
+      // create text field to input user's own portal item ID
+      inputTextField = new TextField("32798dfad17942858d5eef82ee802f0b");
+      inputTextField.setMaxWidth(250);
 
-            // fetch the default feature collection layer from the portal
-            fetchFeatureCollectionFromPortal();
+      // fetch the default feature collection layer from the portal
+      fetchFeatureCollectionFromPortal();
 
-            // create button to perform action
-            Button fetchFromPortalButton = new Button("Open from portal item");
-            fetchFromPortalButton.setMaxWidth(250);
+      // create button to perform action
+      Button fetchFromPortalButton = new Button("Open from portal item");
+      fetchFromPortalButton.setMaxWidth(250);
 
-            // verify the input and fetch the portal item
-            fetchFromPortalButton.setOnAction(e -> fetchFeatureCollectionFromPortal());
+      // verify the input and fetch the portal item
+      fetchFromPortalButton.setOnAction(e -> fetchFeatureCollectionFromPortal());
 
-            // add the map view and control panel to stack pane
-            stackPane.getChildren().addAll(mapView, inputTextField, fetchFromPortalButton);
-            StackPane.setAlignment(inputTextField, Pos.TOP_LEFT);
-            StackPane.setMargin(inputTextField, new Insets(10, 0, 0, 10));
-            StackPane.setAlignment(fetchFromPortalButton, Pos.TOP_LEFT);
-            StackPane.setMargin(fetchFromPortalButton, new Insets(40, 0, 0, 10));
+      // add the map view and control panel to stack pane
+      stackPane.getChildren().addAll(mapView, inputTextField, fetchFromPortalButton);
+      StackPane.setAlignment(inputTextField, Pos.TOP_LEFT);
+      StackPane.setMargin(inputTextField, new Insets(10, 0, 0, 10));
+      StackPane.setAlignment(fetchFromPortalButton, Pos.TOP_LEFT);
+      StackPane.setMargin(fetchFromPortalButton, new Insets(40, 0, 0, 10));
 
-        } catch (Exception e) {
-            // on any error, display the stack trace
-            e.printStackTrace();
-        }
+    } catch (Exception e) {
+      // on any error, display the stack trace
+      e.printStackTrace();
     }
+  }
 
-    /**
-     * Fetch feature collection from portal and load as feature layer.
-     */
-    private void fetchFeatureCollectionFromPortal() {
+  /**
+   * Fetch feature collection from portal and load as feature layer.
+   */
+  private void fetchFeatureCollectionFromPortal() {
 
-        if (!inputTextField.getText().isEmpty()) {
-            // crate portal item
-            portalItem = new PortalItem(portal, inputTextField.getText());
-            portalItem.loadAsync();
+    if (!inputTextField.getText().isEmpty()) {
+      // crate portal item
+      portalItem = new PortalItem(portal, inputTextField.getText());
+      portalItem.loadAsync();
 
-            portalItem.addDoneLoadingListener(() -> {
+      portalItem.addDoneLoadingListener(() -> {
 
-                if (portalItem.getLoadStatus() == LoadStatus.LOADED) {
+        if (portalItem.getLoadStatus() == LoadStatus.LOADED) {
 
-                    if (portalItem.getType() == PortalItem.Type.FEATURE_COLLECTION) {
+          if (portalItem.getType() == PortalItem.Type.FEATURE_COLLECTION) {
 
-                        // create feature collection and add to the map as a layer
-                        FeatureCollection  featureCollection = new FeatureCollection(portalItem);
-                        FeatureCollectionLayer featureCollectionLayer = new FeatureCollectionLayer(featureCollection);
+            // create feature collection and add to the map as a layer
+            FeatureCollection  featureCollection = new FeatureCollection(portalItem);
+            FeatureCollectionLayer featureCollectionLayer = new FeatureCollectionLayer(featureCollection);
 
-                        // add feature collection layer to the map
-                        map.getOperationalLayers().clear();
-                        map.getOperationalLayers().add(featureCollectionLayer);
+            // add feature collection layer to the map
+            map.getOperationalLayers().clear();
+            map.getOperationalLayers().add(featureCollectionLayer);
 
-                        } else {
-                        new Alert(Alert.AlertType.ERROR, "Portal item is not a feature collection").show();
-                    }
-                } else {
-                    new Alert(Alert.AlertType.ERROR, "Portal item failed to load").show();
-                }
-            });
+          } else {
+            new Alert(Alert.AlertType.ERROR, "Portal item is not a feature collection").show();
+          }
         } else {
-            new Alert(Alert.AlertType.ERROR, "Portal Item ID is empty").show();
+          new Alert(Alert.AlertType.ERROR, "Portal item failed to load").show();
         }
-
-    }
-    /**
-     * Stops and releases all resources used in application.
-     */
-    @Override
-    public void stop() {
-
-        if (mapView != null) {
-            mapView.dispose();
-        }
+      });
+    } else {
+      new Alert(Alert.AlertType.ERROR, "Portal Item ID is empty").show();
     }
 
-    /**
-     * Opens and runs application.
-     *
-     * @param args arguments passed to this application
-     */
-    public static void main(String[] args) {
+  }
+  /**
+   * Stops and releases all resources used in application.
+   */
+  @Override
+  public void stop() {
 
-        Application.launch(args);
+    if (mapView != null) {
+      mapView.dispose();
     }
+  }
+
+  /**
+   * Opens and runs application.
+   *
+   * @param args arguments passed to this application
+   */
+  public static void main(String[] args) {
+
+    Application.launch(args);
+  }
 }
