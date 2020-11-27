775bf5e9ddc3a3a64403ee0bcea087887b4dd968
sclaridge <67917108+sclaridge@users.noreply.github.com>
2020-11-27 15:04:45 +0000

    Update RasterLayerFileSample.java

--- raster/raster-layer-file/src/main/java/com/esri/samples/raster_layer_file/RasterLayerFileSample.java
+++ raster/raster-layer-file/src/main/java/com/esri/samples/raster_layer_file/RasterLayerFileSample.java
@@ -24,10 +24,11 @@ import javafx.scene.control.Alert;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
 import com.esri.arcgisruntime.layers.RasterLayer;
 import com.esri.arcgisruntime.loadable.LoadStatus;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
-import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.BasemapStyle;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.raster.Raster;
 
@@ -50,14 +51,18 @@ public class RasterLayerFileSample extends Application {
       stage.setScene(scene);
       stage.show();
 
+      // authentication with an API key or named user is required to access basemaps and other location services
+      String yourAPIKey = System.getProperty("apiKey");
+      ArcGISRuntimeEnvironment.setApiKey(yourAPIKey);
+
       // create a raster from a local raster file
       Raster raster = new Raster(new File(System.getProperty("data.dir"), "./samples-data/raster/Shasta.tif").getAbsolutePath());
 
       // create a raster layer
       RasterLayer rasterLayer = new RasterLayer(raster);
 
-      // create a Map with imagery basemap
-      ArcGISMap map = new ArcGISMap(Basemap.createImagery());
+      // create a map with an imagery basemap style
+      ArcGISMap map = new ArcGISMap(BasemapStyle.ARCGIS_IMAGERY_STANDARD);
 
       // add the map to a map view
       mapView = new MapView();
