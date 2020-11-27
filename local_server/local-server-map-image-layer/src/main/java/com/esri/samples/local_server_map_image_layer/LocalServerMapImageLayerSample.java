4ab5faab5a05a2febdfab3769c54d41c936ba6f2
sclaridge <67917108+sclaridge@users.noreply.github.com>
2020-11-27 11:57:32 +0000

    Update LocalServerMapImageLayerSample.java

--- local_server/local-server-map-image-layer/src/main/java/com/esri/samples/local_server_map_image_layer/LocalServerMapImageLayerSample.java
+++ local_server/local-server-map-image-layer/src/main/java/com/esri/samples/local_server_map_image_layer/LocalServerMapImageLayerSample.java
@@ -28,6 +28,7 @@ import javafx.scene.control.ProgressIndicator;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
 import com.esri.arcgisruntime.geometry.Envelope;
 import com.esri.arcgisruntime.layers.ArcGISMapImageLayer;
 import com.esri.arcgisruntime.loadable.LoadStatus;
@@ -36,7 +37,7 @@ import com.esri.arcgisruntime.localserver.LocalServer;
 import com.esri.arcgisruntime.localserver.LocalServerStatus;
 import com.esri.arcgisruntime.localserver.LocalService.StatusChangedEvent;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
-import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.BasemapStyle;
 import com.esri.arcgisruntime.mapping.Viewpoint;
 import com.esri.arcgisruntime.mapping.view.MapView;
 
@@ -64,8 +65,14 @@ public class LocalServerMapImageLayerSample extends Application {
       stage.setScene(scene);
       stage.show();
 
-      // create a view with a map and basemap
-      ArcGISMap map = new ArcGISMap(Basemap.createStreets());
+      // authentication with an API key or named user is required to access basemaps and other location services
+      String yourAPIKey = System.getProperty("apiKey");
+      ArcGISRuntimeEnvironment.setApiKey(yourAPIKey);
+
+      // create a map with a basemap style
+      ArcGISMap map = new ArcGISMap(BasemapStyle.ARCGIS_STREETS);
+
+      // create a map view and set its map
       mapView = new MapView();
       mapView.setMap(map);
 
