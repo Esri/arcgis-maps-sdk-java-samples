5f17bc824598b5bfc5456f39013247b93586c316
Jen Merritt <56556822+jenmerritt@users.noreply.github.com>
2020-12-04 11:34:33 +0000

    Sketch on map - set viewpoint on mapview

--- display_information/sketch-on-map/src/main/java/com/esri/samples/sketch_on_map/SketchOnMapController.java
+++ display_information/sketch-on-map/src/main/java/com/esri/samples/sketch_on_map/SketchOnMapController.java
@@ -67,12 +67,14 @@ public class SketchOnMapController {
     String yourAPIKey = System.getProperty("apiKey");
     ArcGISRuntimeEnvironment.setApiKey(yourAPIKey);
 
-    // create a map with a basemap style and set an initial viewpoint
+    // create a map with a basemap style
     ArcGISMap map = new ArcGISMap(BasemapStyle.ARCGIS_IMAGERY_STANDARD);
-    map.setInitialViewpoint(new Viewpoint(64.3286, -15.5314, 70000));
 
     // set the map to the map view
     mapView.setMap(map);
+
+    // set a viewpoint on the map view
+    mapView.setViewpoint(new Viewpoint(64.3286, -15.5314, 70000));
 
     // create a graphics overlay for the graphics
     graphicsOverlay = new GraphicsOverlay();
