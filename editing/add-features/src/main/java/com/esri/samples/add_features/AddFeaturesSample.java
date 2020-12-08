bd8d4702ef968f854a2389b4102f07608f74c7f3
Jen Merritt <56556822+jenmerritt@users.noreply.github.com>
2020-12-08 11:21:30 +0000

    Add features - tidy comment

--- editing/add-features/src/main/java/com/esri/samples/add_features/AddFeaturesSample.java
+++ editing/add-features/src/main/java/com/esri/samples/add_features/AddFeaturesSample.java
@@ -72,7 +72,7 @@ public class AddFeaturesSample extends Application {
       String yourAPIKey = System.getProperty("apiKey");
       ArcGISRuntimeEnvironment.setApiKey(yourAPIKey);
 
-      // create a map with streets basemap style
+      // create a map with the streets basemap style
       ArcGISMap map = new ArcGISMap(BasemapStyle.ARCGIS_STREETS);
 
       // create a map view and set its map
