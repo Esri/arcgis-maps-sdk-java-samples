2f0149132b846f732921ef8f54309a3528c69486
Jen Merritt <56556822+jenmerritt@users.noreply.github.com>
2020-12-10 17:33:59 +0000

    restructure and improve comments

--- symbology/create-symbols-from-web-styles/src/main/java/com/esri/samples/create_symbols_from_web_styles/CreateSymbolsFromWebStylesSample.java
+++ symbology/create-symbols-from-web-styles/src/main/java/com/esri/samples/create_symbols_from_web_styles/CreateSymbolsFromWebStylesSample.java
@@ -38,14 +38,13 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
+import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
 import com.esri.arcgisruntime.concurrent.ListenableFuture;
 import com.esri.arcgisruntime.data.ServiceFeatureTable;
-import com.esri.arcgisruntime.geometry.Point;
-import com.esri.arcgisruntime.geometry.SpatialReferences;
 import com.esri.arcgisruntime.layers.FeatureLayer;
 import com.esri.arcgisruntime.loadable.LoadStatus;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
-import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.BasemapStyle;
 import com.esri.arcgisruntime.mapping.Viewpoint;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.symbology.Symbol;
@@ -55,6 +54,8 @@ import com.esri.arcgisruntime.symbology.UniqueValueRenderer;
 public class CreateSymbolsFromWebStylesSample extends Application {
 
   private MapView mapView;
+  private GridPane gridPane;
+  private SymbolStyle symbolStyle;
 
   @Override
   public void start(Stage stage) {
@@ -71,30 +72,51 @@ public class CreateSymbolsFromWebStylesSample extends Application {
       stage.setScene(scene);
       stage.show();
 
-      // create a map with the imagery basemap
-      ArcGISMap map = new ArcGISMap(Basemap.createLightGrayCanvasVector());
+      // authentication with an API key or named user is required to access basemaps and other location services
+      String yourAPIKey = System.getProperty("apiKey");
+      ArcGISRuntimeEnvironment.setApiKey(yourAPIKey);
+
+      // create a map with the light gray basemap style
+      ArcGISMap map = new ArcGISMap(BasemapStyle.ARCGIS_LIGHT_GRAY);
+
+      // set an initial reference scale on the map for controlling symbol size
       map.setReferenceScale(100000);
 
-      // create a map view and set its map
+      // create a map view and set the map to it
       mapView = new MapView();
       mapView.setMap(map);
+
+      // set a viewpoint on the map view
+      mapView.setViewpoint(new Viewpoint(34.28883, -118.43455, 7000));
 
       // create a feature layer from a service
       FeatureLayer featureLayer = new FeatureLayer(new ServiceFeatureTable(
         "http://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/LA_County_Points_of_Interest/FeatureServer/0"));
 
-      // create a unique value renderer and add the relevant field from the feature layer to match symbols with
+      // add the feature layer to the map's operational layers
+      map.getOperationalLayers().add(featureLayer);
+
+      // display an error if the feature layer fails to load
+      featureLayer.addDoneLoadingListener(() -> {
+        if (featureLayer.getLoadStatus() == LoadStatus.FAILED_TO_LOAD) {
+          new Alert(Alert.AlertType.ERROR, "Feature layer failed to load. Details: \n"
+            + featureLayer.getLoadError().getCause().getMessage()).show();
+        }
+      });
+
+      // create a unique value renderer
       UniqueValueRenderer uniqueValueRenderer = new UniqueValueRenderer();
+
+      // add the name of a field from the feature layer data that symbols will be mapped to
       uniqueValueRenderer.getFieldNames().add("cat2");
 
-      // set the renderer on the feature layer
+      // set the unique value renderer on the feature layer
       featureLayer.setRenderer(uniqueValueRenderer);
 
-      // create a symbol style from a portal
-      SymbolStyle symbolStyle = new SymbolStyle("Esri2DPointSymbolsStyle", null);
+      // create a symbol style from a web style
+      symbolStyle = new SymbolStyle("Esri2DPointSymbolsStyle", null);
 
-      // display an error if the symbol style does not load
-      symbolStyle.loadAsync();
+      // display an error if the symbol style fails to load
       symbolStyle.addDoneLoadingListener(() -> {
         if (symbolStyle.getLoadStatus() == LoadStatus.FAILED_TO_LOAD) {
           new Alert(Alert.AlertType.ERROR, "Error: could not load symbol style. Details: \n"
@@ -102,71 +124,59 @@ public class CreateSymbolsFromWebStylesSample extends Application {
         }
       });
 
-      // create a legend
-      GridPane gridPane = new GridPane();
-      gridPane.getColumnConstraints().addAll(Arrays.asList(new ColumnConstraints(70), new ColumnConstraints(120)));
-      Label legendTitle = new Label("Style: " + symbolStyle.getStyleName());
-      legendTitle.setStyle("-fx-font-weight: bold");
-      gridPane.add(legendTitle, 0, 0,2,1);
-      Label symbolTitle = new Label("Symbol");
-      symbolTitle.setStyle("-fx-font-weight: bold");
-      Label nameTitle = new Label("Name");
-      nameTitle.setStyle("-fx-font-weight: bold");
-      gridPane.add(symbolTitle, 0, 1);
-      gridPane.add(nameTitle, 1, 1);
-      gridPane.setMaxWidth(175);
-      gridPane.setMaxHeight(570);
-      gridPane.setBackground(new Background(new BackgroundFill(Paint.valueOf("rgba(255,255,255, 0.9)"), CornerRadii.EMPTY,
-        Insets.EMPTY)));
-      gridPane.setPadding(new Insets(10));
-      gridPane.setVgap(12);
+      // setup the UI for the legend
+      setupLegend();
 
-      ArrayList<String> listOfKeys = new ArrayList<>();
-      listOfKeys.addAll(Arrays.asList("atm", "beach", "campground", "city-hall", "hospital", "library", "park",
+      // create a list of the required symbol names from the web style
+      ArrayList<String> symbolNames = new ArrayList<>();
+      symbolNames.addAll(Arrays.asList("atm", "beach", "campground", "city-hall", "hospital", "library", "park",
         "place-of-worship", "police-station", "post-office", "school", "trail"));
 
-      for (String key : listOfKeys) {
-        List<String> keySearch = new ArrayList<>();
-        keySearch.add(key);
-        ListenableFuture<Symbol> searchResult = symbolStyle.getSymbolAsync(keySearch);
+      for (String symbolName : symbolNames) {
+
+        // search for each symbol in the symbol style
+        List<String> searchKey = new ArrayList<>();
+        searchKey.add(symbolName);
+        ListenableFuture<Symbol> searchResult = symbolStyle.getSymbolAsync(searchKey);
         searchResult.addDoneListener(() -> {
           try {
+            // get the symbol from the search result
             Symbol symbol = searchResult.get();
-            ImageView imageView = addSymbolToImageView(symbol);
-            Label gridPaneLabel = new Label(key);
-            gridPane.add(imageView, 0, listOfKeys.indexOf(key)+2);
-            gridPane.add(gridPaneLabel, 1, listOfKeys.indexOf(key)+2);
-            List<String> categories = mapSymbolKeyToCategories(key);
+
+            // get a list of all categories to be mapped to the symbol
+            List<String> categories = mapSymbolNameToField(symbolName);
+
             for (String category : categories) {
+
+              // create a unique value for each category
               List<Object> categoryValue = new ArrayList<>();
               categoryValue.add(category);
-              UniqueValueRenderer.UniqueValue uniqueValue = new UniqueValueRenderer.UniqueValue("", key, symbol, categoryValue);
+              UniqueValueRenderer.UniqueValue uniqueValue = new UniqueValueRenderer.UniqueValue(
+                "", symbolName, symbol, categoryValue);
+
+              // add each unique value to the unique value renderer
               uniqueValueRenderer.getUniqueValues().add(uniqueValue);
             }
+
+            // create and add an image view and a label for the symbol to the legend on the UI
+            ImageView imageView = createImageView(symbol);
+            Label gridPaneLabel = new Label(symbolName);
+            gridPane.add(imageView, 0, symbolNames.indexOf(symbolName) + 2);
+            gridPane.add(gridPaneLabel, 1, symbolNames.indexOf(symbolName) + 2);
+
           } catch (Exception e) {
-            // on any error, display the stack trace
+            // on any error, display the stack trace.
             e.printStackTrace();
           }
         });
       }
 
-      // add the feature layer to the map
-      map.getOperationalLayers().add(featureLayer);
-
-      featureLayer.addDoneLoadingListener(() -> {
-        if (featureLayer.getLoadStatus() == LoadStatus.LOADED) {
-          // set the map views's viewpoint centered on Los Angeles, California and scaled
-          mapView.setViewpoint(new Viewpoint(new Point(-13184975, 4066890,
-            SpatialReferences.getWebMercator()), 7000));
-        } else new Alert(Alert.AlertType.ERROR, "Feature Layer Failed to Load!\n" +
-          featureLayer.getLoadError().getCause().getMessage()).show();
-      });
-
+      // add a map scale changed listener on the map view to control the symbol sizes at different scales
       mapView.addMapScaleChangedListener(mapScaleChangedEvent -> {
         featureLayer.setScaleSymbols(!(mapView.getMapScale() < 80000));
       });
 
-      // add the map view to stack pane
+      // add the map view and grid pane to the stack pane
       stackPane.getChildren().addAll(mapView, gridPane);
       StackPane.setAlignment(gridPane, Pos.TOP_LEFT);
       StackPane.setMargin(gridPane, new Insets(10, 0, 0, 10));
@@ -175,19 +185,44 @@ public class CreateSymbolsFromWebStylesSample extends Application {
       // on any error, display the stack trace.
       e.printStackTrace();
     }
+
   }
 
   /**
-   * Returns a list of categories to be matched to a symbol key.
+   * Creates a legend on the UI for the symbol styles.
    *
-   * @param key the name of a symbol
-   * @return categories a list of categories matched to the provided symbol key
    */
-  private List<String> mapSymbolKeyToCategories(String key) {
+  private void setupLegend() {
+    gridPane = new GridPane();
+    gridPane.getColumnConstraints().addAll(Arrays.asList(new ColumnConstraints(70), new ColumnConstraints(120)));
+    Label legendTitle = new Label("Style: " + symbolStyle.getStyleName());
+    legendTitle.setStyle("-fx-font-weight: bold");
+    gridPane.add(legendTitle, 0, 0,2,1);
+    Label symbolTitle = new Label("Symbol");
+    symbolTitle.setStyle("-fx-font-weight: bold");
+    Label nameTitle = new Label("Name");
+    nameTitle.setStyle("-fx-font-weight: bold");
+    gridPane.add(symbolTitle, 0, 1);
+    gridPane.add(nameTitle, 1, 1);
+    gridPane.setMaxWidth(175);
+    gridPane.setMaxHeight(570);
+    gridPane.setBackground(new Background(new BackgroundFill(Paint.valueOf("rgba(255,255,255, 0.9)"), CornerRadii.EMPTY,
+      Insets.EMPTY)));
+    gridPane.setPadding(new Insets(10));
+    gridPane.setVgap(12);
+  }
+
+  /**
+   * Returns a list of categories to be matched to a symbol name.
+   *
+   * @param symbolName the name of a symbol from a symbol style
+   * @return categories a list of categories matched to the provided symbol name
+   */
+  private List<String> mapSymbolNameToField(String symbolName) {
 
     List<String> categories = new ArrayList<>();
 
-    switch (key) {
+    switch (symbolName) {
       case "atm":
         categories.add("Banking and Finance");
         break;
@@ -235,15 +270,16 @@ public class CreateSymbolsFromWebStylesSample extends Application {
    * @param symbol the symbol to display in the imageView
    * @return imageView the imageView populated with the symbol
    */
-  private ImageView addSymbolToImageView(Symbol symbol) {
+  private ImageView createImageView(Symbol symbol) {
 
-    // add the symbol to the image view to display it in the legend
+    // create an image view for displaying the symbol in the legend
     ImageView imageView = new ImageView();
-    ListenableFuture<Image> image = symbol.createSwatchAsync(0x00000000);
-    image.addDoneListener(() -> {
+    ListenableFuture<Image> imageOfSymbol = symbol.createSwatchAsync(0x00000000);
+    imageOfSymbol.addDoneListener(() -> {
       try {
-        // display the image in the image view
-        imageView.setImage(image.get());
+        // add the symbol image to the image view
+        imageView.setImage(imageOfSymbol.get());
+        System.out.println(imageOfSymbol.getClass());
       } catch (InterruptedException | ExecutionException e) {
         new Alert(Alert.AlertType.ERROR, "Error creating preview ImageView from provided symbol").show();
       }
