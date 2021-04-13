40124410c3f09da3daf19be919cadbb9dbf2efdf
Rachael Ellen <rach9955@esri.com>
2021-04-13 11:45:45 +0100

    sample set up

---
+++ map_view/display-device-location-with-nmea-data-sources/src/main/java/com/esri/samples/display_device_location_with_nmea_data_sources/DisplayDeviceLocationWithNmeaDataSourcesController.java
@@ -0,0 +1,219 @@
+/*
+ * Copyright 2021 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.esri.samples.display_device_location_with_nmea_data_sources;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.esri.arcgisruntime.location.NmeaLocationDataSource;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+
+import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
+import com.esri.arcgisruntime.geometry.Geometry;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.geometry.Polyline;
+import com.esri.arcgisruntime.geometry.PolylineBuilder;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
+import com.esri.arcgisruntime.loadable.LoadStatus;
+import com.esri.arcgisruntime.location.LocationDataSource;
+import com.esri.arcgisruntime.location.LocationDataSource.LocationChangedListener;
+import com.esri.arcgisruntime.location.SimulatedLocationDataSource;
+import com.esri.arcgisruntime.location.SimulationParameters;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.BasemapStyle;
+import com.esri.arcgisruntime.mapping.Viewpoint;
+import com.esri.arcgisruntime.mapping.view.Graphic;
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
+import com.esri.arcgisruntime.mapping.view.LocationDisplay;
+import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.symbology.SimpleLineSymbol;
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
+import com.esri.arcgisruntime.symbology.SimpleRenderer;
+
+import javafx.util.Duration;
+import org.apache.commons.io.IOUtils;
+
+public class DisplayDeviceLocationWithNmeaDataSourcesController {
+
+  @FXML private MapView mapView;
+  private NmeaLocationDataSource nmeaLocationDataSource;
+  private LocationDisplay locationDisplay;
+  private List<String> nmeaData;
+  private int count = 0;
+  @FXML private Label satelliteCount;
+  @FXML private Label systemInfo;
+  @FXML private Label satelliteID;
+  @FXML private Button startButton;
+  @FXML private Button resetButton;
+  @FXML private Button recenterButton;
+
+  private Point position;
+
+  public void initialize() {
+
+    try {
+
+      // authentication with an API key or named user is required to access basemaps and other location services
+      String yourAPIKey = System.getProperty("apiKey");
+      ArcGISRuntimeEnvironment.setApiKey(yourAPIKey);
+
+      // create a map with the dark gray basemap style
+      ArcGISMap map = new ArcGISMap(BasemapStyle.ARCGIS_NAVIGATION);
+
+      // create a map view and set the map to it
+      mapView.setMap(map);
+
+      // set a viewpoint on the map view centered on Los Angeles, California
+      mapView.setViewpoint(new Viewpoint(new Point(-117.191, 34.0306, SpatialReferences.getWgs84()), 100000));
+
+      // create a new NMEA location data source
+      nmeaLocationDataSource = new NmeaLocationDataSource(SpatialReferences.getWgs84());
+      locationDisplay = mapView.getLocationDisplay();
+      locationDisplay.setLocationDataSource(nmeaLocationDataSource);
+      locationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.RECENTER);
+
+      start();
+
+
+
+
+    } catch (Exception e) {
+      // on any error, display the stack trace.
+      e.printStackTrace();
+    }
+  }
+
+  @FXML
+  private void start () throws IOException {
+
+    // enable receiving NMEA location data from external device
+    nmeaLocationDataSource.startAsync();
+    // display the user's location
+    locationDisplay.startAsync();
+
+    // load simulated NMEA sentences for sample
+    File simulatedNmeaDataFile = new File(System.getProperty("data.dir"), "./samples-data/redlands/Redlands.nmea");
+    if (simulatedNmeaDataFile.exists()) {
+
+      try  {
+        List<String> lines = Files.readAllLines(simulatedNmeaDataFile.toPath(), StandardCharsets.UTF_8);
+
+        List<String> sortedLines = new ArrayList<>();
+
+        lines.forEach(line -> {
+          if (line.startsWith("$GPGGA")) {
+            sortedLines.add(line);
+          } else {
+            int index = sortedLines.size() - 1;
+            sortedLines.add(index, line + "\n" );
+          }
+
+        });
+
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(-1); // loop count
+        timeline.getKeyFrames().add(new KeyFrame(Duration.millis(), event -> {
+          String nmeaLine = sortedLines.get(count);
+          nmeaLocationDataSource.pushData(sortedLines.get(count++).getBytes(StandardCharsets.UTF_8)); // post increment step
+//          count++;
+          System.out.println(count);
+          System.out.println(nmeaLine);
+          if (count >= lines.size()) count = 0;
+
+        }));
+        timeline.play();
+
+
+
+        System.out.println(sortedLines);
+
+
+
+//        nmeaLocationDataSource.pushData(lines.get(1).getBytes(StandardCharsets.UTF_8));
+
+
+//        lines.forEach(line -> {
+//          nmeaLocationDataSource.pushData(line.getBytes(StandardCharsets.UTF_8));
+//
+//        });
+//        nmeaData =  lines.collect(Collectors.toList());
+//        System.out.println(nmeaData.size());
+
+//        nmeaLocationDataSource.pushData(nmeaData.get(1).getBytes(StandardCharsets.UTF_8));
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+
+//      String content = Files.readString(simulatedNmeaDataFile.toPath(), StandardCharsets.UTF_8);
+//      List<String> splitStrings = Arrays.asList(content.split("\n").clone());
+//      splitStrings.forEach(line -> {
+//        if (line.startsWith("$GPGGA")) {
+//          nmeaData.add(line + "\n");
+//        } else {
+//          nmeaData.set(nmeaData.size() - 1, (line + "\n"));
+//        }
+//
+//        System.out.println(nmeaData);
+//      });
+
+//      System.out.println(Arrays.toString(content.getBytes(StandardCharsets.UTF_8)));
+//
+//      nmeaLocationDataSource.pushData(content.getBytes(StandardCharsets.UTF_8));
+//
+
+    } else {
+      new Alert(Alert.AlertType.ERROR, "File not found").show();
+    }
+
+
+
+
+
+
+  }
+
+
+  /**
+   * Disposes application resources.
+   */
+  void terminate() {
+    if (mapView != null) {
+      mapView.dispose();
+    }
+  }
+
+
+}
