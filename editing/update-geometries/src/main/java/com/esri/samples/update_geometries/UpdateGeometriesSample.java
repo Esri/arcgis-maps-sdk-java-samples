010ecbd0637424c5064701d79bd7d3268b9819f0
Rachael Ellen <rellen@esri.com>
2022-04-13 12:06:07 +0100

    Update editing/update-geometries/src/main/java/com/esri/samples/update_geometries/UpdateGeometriesSample.java
    Co-authored-by: Jen Merritt <jmerritt@esri.com>

--- editing/update-geometries/src/main/java/com/esri/samples/update_geometries/UpdateGeometriesSample.java
+++ editing/update-geometries/src/main/java/com/esri/samples/update_geometries/UpdateGeometriesSample.java
@@ -92,7 +92,7 @@ public class UpdateGeometriesSample extends Application {
       var serviceGeodatabase = new ServiceGeodatabase(FEATURE_LAYER_URL);
       serviceGeodatabase.addDoneLoadingListener(() -> {
 
-        // create service feature table from the service geodatabase's table first layer
+        // create a service feature table from the first layer of the service geodatabase's table
         featureTable = serviceGeodatabase.getTable(0);
 
         // create a feature layer from table
