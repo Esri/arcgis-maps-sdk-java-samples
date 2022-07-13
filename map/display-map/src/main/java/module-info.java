1e896e2729ce944a42bdc33d7d8e780a4c9efea2
mbcoder <mbaird@esri.com>
2022-07-13 13:54:42 +0100

    Changes with working API

--- map/display-map/src/main/java/module-info.java
+++ map/display-map/src/main/java/module-info.java
@@ -1,12 +1,9 @@
 module display.map.main {
   requires com.esri.arcgisruntime;
-
   requires javafx.controls;
-  requires javafx.base;
   requires javafx.graphics;
   requires javafx.media;
   requires javafx.web;
   requires javafx.fxml;
-
   exports com.esri.samples.display_map;
 }
