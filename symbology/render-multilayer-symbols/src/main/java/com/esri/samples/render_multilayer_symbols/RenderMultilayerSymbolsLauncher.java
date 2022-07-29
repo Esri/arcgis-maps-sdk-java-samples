41e33f3ab0bdded4ce0c13528fbf9bde9f7c11d8
Oliver Smith <67012037+01smito01@users.noreply.github.com>
2022-07-29 12:32:33 +0100

    [New Sample] Render Multilayer Symbols (#693)
    * [New Sample] Render Multilayer Symbols
    
    * Add loading listeners to images
    
    * Fixed imports, made label text consistent, updated README
    
    * examples of condensing code
    
    * Reduced code duplication, added javadocs, updated README
    
    * Fixed metadata (oops)
    
    * Implemented changes from PR review
    
    * Implement (most) changes from PR review
    
    * Moved async code from addGraphicFromPictureMarkerSymbolLayer to start
    
    Co-authored-by: Rachael Ellen <rellen@esri.com>

---
+++ symbology/render-multilayer-symbols/src/main/java/com/esri/samples/render_multilayer_symbols/RenderMultilayerSymbolsLauncher.java
@@ -0,0 +1,11 @@
+package com.esri.samples.render_multilayer_symbols;
+
+/**
+* Wrapper required for launching a JavaFX 11 app through Gradle or from a jar.
+*/
+public class RenderMultilayerSymbolsLauncher {
+
+  public static void main(String[] args) {
+    RenderMultilayerSymbolsSample.main(args);
+  }
+}
