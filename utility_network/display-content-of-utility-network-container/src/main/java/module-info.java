1cbe7cb34a3839edb94dd4fbb75db3e7dc48bac4
Luis Flores Carrubio <lflores@esri.com>
2022-09-22 10:10:40 +0100

    [Update] Utility network sample category v200-beta (#713)

--- utility_network/display-content-of-utility-network-container/src/main/java/module-info.java
+++ utility_network/display-content-of-utility-network-container/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Esri.
+ * Copyright 2022 Esri.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,14 +14,17 @@
  * limitations under the License.
  */
 
-package com.esri.samples.display_content_of_utility_network_container;
+module display.content.of.utility.network.container.main {
+  // require ArcGIS Runtime module
+  requires com.esri.arcgisruntime;
 
-/**
- * Wrapper required for launching a JavaFX 11 app through Gradle or from a jar.
- */
-public class DisplayContentOfUtilityNetworkContainerLauncher {
+  // require JavaFX modules
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires javafx.graphics;
 
-  public static void main(String[] args) {
-    DisplayContentOfUtilityNetworkContainerSample.main(args);
-  }
+  // make all annotated objects reflectively accessible to the javafx.fxml module
+  opens com.esri.samples.display_content_of_utility_network_container to javafx.fxml;
+
+  exports com.esri.samples.display_content_of_utility_network_container;
 }
