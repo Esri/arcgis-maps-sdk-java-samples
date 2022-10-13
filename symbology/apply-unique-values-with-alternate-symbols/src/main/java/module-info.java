3c67521c60f525f57a7457086a184caaaf1b22ab
Jen Merritt <jmerritt@esri.com>
2022-10-13 16:13:26 +0100

    update apply unique values

--- symbology/apply-unique-values-with-alternate-symbols/src/main/java/module-info.java
+++ symbology/apply-unique-values-with-alternate-symbols/src/main/java/module-info.java
@@ -13,11 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-module apply.unique.values.with.alternate.symbols.main {
+module com.esri.samples.apply_unique_values_with_alternate_symbols {
   // require ArcGIS Runtime module
   requires com.esri.arcgisruntime;
 
-  // require JavaFX modules
+  // require JavaFX modules that the application uses
   requires javafx.graphics;
   requires javafx.controls;
 
