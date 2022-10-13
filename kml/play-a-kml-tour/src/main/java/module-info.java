e58aef685ba3148b6244b13c2ddc0d6336cc13c6
Jen Merritt <jmerritt@esri.com>
2022-10-13 19:05:12 +0100

    update play a kml tour

--- kml/play-a-kml-tour/src/main/java/module-info.java
+++ kml/play-a-kml-tour/src/main/java/module-info.java
@@ -13,11 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-module play.a.kml.tour.main {
+module com.esri.samples.play_a_kml_tour {
   // require ArcGIS Runtime module
   requires com.esri.arcgisruntime;
 
-  // require JavaFX modules
+  // require JavaFX modules that the application uses
   requires javafx.graphics;
   requires javafx.controls;
 
