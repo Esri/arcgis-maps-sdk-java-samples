237a307ac14c1d64de91560edc564476c83b2451
Jen Merritt <jmerritt@esri.com>
2022-10-13 19:27:36 +0100

    update animate 3d graphic

--- scene/animate-3d-graphic/src/main/java/module-info.java
+++ scene/animate-3d-graphic/src/main/java/module-info.java
@@ -13,11 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-module animate._3d.graphic.main {
+module com.esri.samples.animate_3d_graphic {
   // require ArcGIS Runtime module
   requires com.esri.arcgisruntime;
 
-  // require JavaFX modules
+  // require JavaFX modules that the application uses
   requires javafx.graphics;
   requires javafx.controls;
   requires javafx.fxml;
