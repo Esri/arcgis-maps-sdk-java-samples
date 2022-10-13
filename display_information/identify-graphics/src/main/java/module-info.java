e1c6b3dd24a7283fcf5872a8657a39f8c85a0758
Jen Merritt <jmerritt@esri.com>
2022-10-13 18:58:38 +0100

    update identify graphics

--- display_information/identify-graphics/src/main/java/module-info.java
+++ display_information/identify-graphics/src/main/java/module-info.java
@@ -13,11 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-module identify.graphics.main {
+module com.esri.samples.identify_graphics {
   // require ArcGIS Runtime module
   requires com.esri.arcgisruntime;
 
-  // require JavaFX modules
+  // require JavaFX modules that the application uses
   requires javafx.graphics;
   requires javafx.controls;
 
