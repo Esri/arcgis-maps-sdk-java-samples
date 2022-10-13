71d379f590b1470987a241932916448260f42937
Jen Merritt <jmerritt@esri.com>
2022-10-13 19:49:36 +0100

    update configure subnetwork trace

--- utility_network/configure-subnetwork-trace/src/main/java/module-info.java
+++ utility_network/configure-subnetwork-trace/src/main/java/module-info.java
@@ -14,16 +14,16 @@
  * limitations under the License.
  */
 
-module configure.subnetwork.trace.main {
+module com.esri.samples.configure_subnetwork_trace {
   // require ArcGIS Runtime module
   requires com.esri.arcgisruntime;
 
-  // require JavaFX modules
+  // require JavaFX modules that the application uses
+  requires javafx.graphics;
   requires javafx.controls;
   requires javafx.fxml;
-  requires javafx.graphics;
 
-  // make all annotated objects reflectively accessible to the javafx.fxml module
+  // make all @FXML annotated objects reflectively accessible to the javafx.fxml module
   opens com.esri.samples.configure_subnetwork_trace to javafx.fxml;
 
   exports com.esri.samples.configure_subnetwork_trace;
