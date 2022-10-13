10c0a513d2de6a956663876f58610d82fa117725
Jen Merritt <jmerritt@esri.com>
2022-10-13 19:32:12 +0100

    update display scene

--- scene/display-scene/src/main/java/module-info.java
+++ scene/display-scene/src/main/java/module-info.java
@@ -13,11 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-module display.scene.main {
+module com.esri.samples.display_scene {
   // require ArcGIS Runtime module
   requires com.esri.arcgisruntime;
 
-  // require JavaFX modules
+  // require JavaFX modules that the application uses
   requires javafx.graphics;
 
   exports com.esri.samples.display_scene;
