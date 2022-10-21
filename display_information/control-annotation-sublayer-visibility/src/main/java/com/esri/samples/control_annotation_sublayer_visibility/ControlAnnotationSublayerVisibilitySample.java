788365c49a1f47d30e73ba0e80005016116d0b91
Luis Flores Carrubio <lflores@esri.com>
2022-10-21 15:44:01 +0100

    reverting usage of mapScale property

--- display_information/control-annotation-sublayer-visibility/src/main/java/com/esri/samples/control_annotation_sublayer_visibility/ControlAnnotationSublayerVisibilitySample.java
+++ display_information/control-annotation-sublayer-visibility/src/main/java/com/esri/samples/control_annotation_sublayer_visibility/ControlAnnotationSublayerVisibilitySample.java
@@ -80,11 +80,9 @@ public class ControlAnnotationSublayerVisibilitySample extends Application {
 
       // show current map scale in a label within the control panel
       Label currentMapScaleLabel = new Label();
-
-      // listen for map scale changes and update the label
-      mapView.mapScaleProperty().addListener((observable, oldValue, newValue) -> {
-        currentMapScaleLabel.setText("Scale: 1:" + Math.round((double) newValue));
-      });
+      mapView.addMapScaleChangedListener(mapScaleChangedEvent ->
+        currentMapScaleLabel.setText("Scale: 1:" + Math.round(mapView.getMapScale()))
+      );
 
       // add checkboxes and label to the control panel
       controlsVBox.getChildren().addAll(closedSublayerCheckbox, openSublayerCheckbox, currentMapScaleLabel);
@@ -116,21 +114,22 @@ public class ControlAnnotationSublayerVisibilitySample extends Application {
                   closedSublayerCheckbox.setOnAction(event -> closedSublayer.setVisible(closedSublayerCheckbox.isSelected()));
                   openSublayerCheckbox.setOnAction(event -> openSublayer.setVisible(openSublayerCheckbox.isSelected()));
 
-                  // listen for map scale changes
-                  mapView.mapScaleProperty().addListener(((observable, oldValue, newValue) -> {
-                    // gray out the open sublayer when the layer is out of scale
-                    if (openSublayer.isVisibleAtScale((double) newValue)) {
+                  // gray out the open sublayer when the layer is out of scale
+                  mapView.addMapScaleChangedListener(mapScaleChangedEvent -> {
+                    if (openSublayer.isVisibleAtScale(mapView.getMapScale())) {
                       openSublayerCheckbox.setStyle("-fx-text-fill: white");
                     } else {
                       openSublayerCheckbox.setStyle("-fx-text-fill: darkgrey");
                     }
-                  }));
+                  });
+
                 } else {
                   new Alert(Alert.AlertType.ERROR, "Error loading Annotation Layer " + layer.getName()).show();
                 }
               });
             }
           }
+
         } else {
           new Alert(Alert.AlertType.ERROR, "Mobile Map Package failed to load.").show();
         }
