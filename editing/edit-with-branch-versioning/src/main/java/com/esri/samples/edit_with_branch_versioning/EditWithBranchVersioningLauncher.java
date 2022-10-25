24dec17d8353806ac68a88511923c0369c674112
Jen Merritt <jmerritt@esri.com>
2022-10-25 12:40:21 +0100

    Modularize Editing Category (#740)
    * modularize editing category
    
    * fix lines
    
    * update gradle version

--- editing/edit-with-branch-versioning/src/main/java/com/esri/samples/edit_with_branch_versioning/EditWithBranchVersioningLauncher.java
+++
@@ -1,9 +0,0 @@
-package com.esri.samples.edit_with_branch_versioning;
-
-/**
- * Wrapper required for launching a JavaFX 11 app through Gradle or from a jar.
- */
-public class EditWithBranchVersioningLauncher {
-
-  public static void main(String[] args) { EditWithBranchVersioningSample.main(args); }
-}
