de33e943f7492caa237036c4a9f350253923b3f7
Jen Merritt <jmerritt@esri.com>
2022-10-31 15:15:35 +0000

    modularize map view

--- map_view/display-device-location-with-nmea-data-sources/src/main/java/com/esri/samples/display_device_location_with_nmea_data_sources/DisplayDeviceLocationWithNmeaDataSourcesLauncher.java
+++
@@ -1,8 +0,0 @@
-package com.esri.samples.display_device_location_with_nmea_data_sources;
-
-/**
- * Wrapper required for launching a JavaFX 11 app through Gradle or from a jar.
- */
-public class DisplayDeviceLocationWithNmeaDataSourcesLauncher {
-  public static void main(String[] args) { DisplayDeviceLocationWithNmeaDataSourcesSample.main(args); }
-}
